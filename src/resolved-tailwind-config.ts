78e9e26d376e57bd076e7614fb956020be8150d9
Jake Strawn <jake.strawn@gmail.com>
2021-06-26 20:43:23 -0400

    chore(deps): upgrading deps & formatting

--- src/resolved-tailwind-config.ts
+++ src/resolved-tailwind-config.ts
@@ -884,11 +884,9 @@ export default {
     'sm': '0 1px 2px 0 rgba(0, 0, 0, 0.05)',
     'DEFAULT':
       '0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06)',
-    'md':
-      '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
+    'md': '0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06)',
     'lg': '0px 8px 32px rgba(0, 0, 0, 0.06)',
-    'xl':
-      '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
+    'xl': '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
     '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
     'inner': 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
     'none': 'none',
@@ -926,6 +924,12 @@ export default {
       warningText: 'var(--ui-warningText)',
       infoText: 'var(--ui-infoText)',
       successText: 'var(--ui-successText)',
+    },
+    demo: {
+      darkBlue: 'var(--demo-darkBlue)',
+      lightBlue: 'var(--demo-lightBlue)',
+      darkGray: 'var(--demo-darkGray)',
+      lightGray: 'var(--demo-lightGray)',
     },
     gray: {
       '50': 'var(--gray-50)',
