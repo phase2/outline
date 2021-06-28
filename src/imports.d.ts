56a9c260e8e9c3464ef5b7e0ca6391fb11915a66
himerus <jake.strawn@gmail.com>
2021-06-28 13:17:58 -0400

    fix: fixing svg support in typescript

--- src/imports.d.ts
+++ src/imports.d.ts
@@ -6,11 +6,7 @@ declare module '*.jpg' {
   const value: string;
   export default value;
 }
-// declare module '*.css' {
-//   const componentStyles: string;
-//   export default componentStyles;
-// }
-// declare module '*.scss' {
-//   const componentStyles: string;
-//   export default componentStyles;
-// }
+declare module '*.svg' {
+  const src: string;
+  export default src;
+}
