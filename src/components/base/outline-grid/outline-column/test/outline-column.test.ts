0fa5a2e74e36955f2d04e701ea51f5396b39a65a
Josh Mabry <jmabry@phase2technology>
2021-06-29 17:49:57 -0700

    feat(testing): add tests to outline-grid

---
+++ src/components/base/outline-grid/outline-column/test/outline-column.test.ts
@@ -0,0 +1,34 @@
+import { OutlineColumn } from '../outline-column';
+import { assert, fixture, html } from '@open-wc/testing';
+
+describe('outline-column', () => {
+  it('is defined', () => {
+    const el = document.createElement('outline-column');
+    assert.instanceOf(el, OutlineColumn);
+  });
+
+  it('renders with default values', async () => {
+    const el = await fixture(html`<outline-column></outline-column>`);
+    assert.shadowDom.equal(
+      el,
+      `
+      <slot>
+      </slot>
+    `
+    );
+  });
+
+  it('renders slotted content', async () => {
+    const el = await fixture(
+      html`<outline-column><p>Testing column</p></outline-column>`
+    );
+    assert.lightDom.equal(
+      el,
+      `
+      <p>
+        Testing column
+      </p>
+      `
+    );
+  });
+});
