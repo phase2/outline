dce2f43bf81806d47fa37383b297c9ec747d2df3
Dan Montgomery <dmontgomery@phase2technology.com>
2021-07-12 17:03:18 -0500

    45: Add modal open / close behavior.

--- src/components/base/outline-modal/outline-modal.ts
+++ src/components/base/outline-modal/outline-modal.ts
@@ -1,5 +1,5 @@
 import { html, TemplateResult, CSSResultGroup } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { customElement, property, state } from 'lit/decorators.js';
 import componentStyles from './outline-modal.css.lit';
 import { OutlineElement } from '../outline-element/outline-element';
 
@@ -17,19 +17,44 @@ export class OutlineModal extends OutlineElement {
   /**
    * The size of the modal.
    */
-  @property({ type: String }) size?: ModalSize = 'medium';
+  @property({ type: String })
+  size?: ModalSize = 'medium';
+
+  @state()
+  _isOpen = false;
+
+  private _handleModalTrigger(): void {
+    this._isOpen = true;
+  }
+
+  private _handleModalClose(): void {
+    this._isOpen = false;
+  }
+
+  private _overlayTemplate(): TemplateResult {
+    let template = html``;
+
+    if (this._isOpen) {
+      template = html`
+        <div id="overlay" class="${this.size}">
+          <div id="close" @click="${this._handleModalClose}">Close</div>
+          <div id="header">
+            <slot name="outline-modal--header"></slot>
+          </div>
+          <slot></slot>
+        </div>
+      `;
+    }
+
+    return template;
+  }
 
   render(): TemplateResult {
     return html`
-      <div id="trigger">
+      <div id="trigger" @click="${this._handleModalTrigger}">
         <slot name="outline-modal--trigger"></slot>
       </div>
-      <div id="overlay" class="${this.size}">
-        <div id="header">
-          <slot name="outline-modal--header"></slot>
-        </div>
-        <slot></slot>
-      </div>
+      ${this._overlayTemplate()}
     `;
   }
 }
