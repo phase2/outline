22bed124f3991bac17f36a778632a4f649fc5d11
Dan Montgomery <dmontgomery@phase2technology.com>
2021-07-12 16:31:13 -0500

    45: Update stories for modal.
    - Use outline components which are styled.
    - Show use of slots.

--- src/components/base/outline-modal/outline-modal.stories.ts
+++ src/components/base/outline-modal/outline-modal.stories.ts
@@ -10,26 +10,22 @@ export default {
   argTypes: {
     triggerSlot: {
       name: 'slot="outline-modal--trigger"',
-      description: 'The trigger slot.',
-      // @todo how can we edit this as HTML.
-      control: { type: 'text' },
+      description: 'The trigger slot. For example, an "open modal" button.',
+      // We aren't setting a control type here so we can edit the value using the infered object.
     },
     headerSlot: {
       name: 'slot="outline-modal--header"',
-      description: 'The header slot.',
-      // @todo how can we edit this as HTML.
-      control: { type: 'text' },
+      description: 'The header slot. For example a header title.',
+      // We aren't setting a control type here so we can edit the value using the infered object.
     },
     // This is the modal content.
-    defaultSlot: {
-      ...argTypeSlotContent.defaultSlot,
-      // @todo how can we edit this as HTML.
-      control: { type: 'text' },
-    },
+    // We aren't setting a control type here so we can edit the value using the infered object.
+    ...argTypeSlotContent,
     size: {
       name: 'size',
       description: 'The size of the modal.',
       options: modalSizes,
+      control: { type: 'select' },
     },
   },
 };
@@ -42,33 +38,69 @@ const Template = ({
 }): TemplateResult => {
   return html`
     <outline-modal size="${ifDefined(size)}">
-      <div slot="outline-modal--trigger">${triggerSlot}</div>
-      <div slot="outline-modal--header">${headerSlot}</div>
-      ${defaultSlot}
+      ${ifDefined(triggerSlot)} ${ifDefined(headerSlot)}
+      ${ifDefined(defaultSlot)}
     </outline-modal>
   `;
 };
 
 export const Small = Template.bind({});
 Small.args = {
-  triggerSlot: html`<a href="#">Open modal</a>`,
-  headerSlot: html`<h2>The modal header</h2>`,
+  triggerSlot: html`
+    <outline-link slot="outline-modal--trigger">
+      Open small modal
+    </outline-link>
+  `,
+  headerSlot: html`
+    <outline-heading slot="outline-modal--header">
+      The modal header
+    </outline-heading>
+  `,
   defaultSlot: html`<p>A small modal.</p>`,
   size: 'small',
 };
 
 export const Medium = Template.bind({});
 Medium.args = {
-  triggerSlot: html`<a href="#">Open modal</a>`,
-  headerSlot: html`<h2>The modal header</h2>`,
+  triggerSlot: html`
+    <outline-link slot="outline-modal--trigger">
+      Open medium modal
+    </outline-link>
+  `,
+  headerSlot: html` <outline-heading slot="outline-modal--header">
+    The modal header
+  </outline-heading>`,
   defaultSlot: html`<p>A medium modal.</p>`,
   size: 'medium',
 };
 
 export const FullScreen = Template.bind({});
 FullScreen.args = {
-  triggerSlot: html`<a href="#">Open modal</a>`,
-  headerSlot: html`<h2>The modal header</h2>`,
+  triggerSlot: html`
+    <outline-link slot="outline-modal--trigger">
+      Open full screen modal
+    </outline-link>
+  `,
+  headerSlot: html`
+    <outline-heading slot="outline-modal--header">
+      The modal header
+    </outline-heading>
+  `,
   defaultSlot: html`<p>A full screen modal.</p>`,
   size: 'full-screen',
+};
+
+export const ButtonTrigger = Template.bind({});
+ButtonTrigger.args = {
+  triggerSlot: html`
+    <outline-button slot="outline-modal--trigger">
+      Open modal with button trigger
+    </outline-button>
+  `,
+  headerSlot: html`
+    <outline-heading slot="outline-modal--header">
+      The modal header
+    </outline-heading>
+  `,
+  defaultSlot: html`<p>A small modal.</p>`,
 };
