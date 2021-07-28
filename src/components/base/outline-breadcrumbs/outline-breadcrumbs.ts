df4b8b5be7934f9c7018399b5b8780a3b678e6fa
Sarah P <lovesarahinc@gmail.com>
2021-07-28 12:00:43 -0400

    feat(breadcrumb): starting on a proper branch

---
+++ src/components/base/outline-breadcrumbs/outline-breadcrumbs.ts
@@ -0,0 +1,26 @@
+// Our base component, which all others extend.
+import { OutlineElement } from '../outline-element/outline-element';
+import { html, TemplateResult } from 'lit';
+import { customElement } from 'lit/decorators.js';
+// import componentStyles from './outline-breadcrumbs.css.lit';
+
+/**
+ * The Outline  Breadcrumbs component
+ * @slot - The default and used twice for this element.
+ */
+
+@customElement('outline-breadcrumbs')
+export class OutlineBreadcrumbs extends OutlineElement {
+  // /**
+  //  * Crumbs Array
+  //  */
+  // @property({ type: Array })
+  // crumbsLinks: HTMLElement[] | null;
+
+  render(): TemplateResult {
+    return html` <nav role="navigation" aria-label="breadcrumbs"></nav>
+      <ol>
+        <slot></slot>
+      </ol>`;
+  }
+}
