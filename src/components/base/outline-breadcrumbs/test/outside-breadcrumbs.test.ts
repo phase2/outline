8f2a117a384e3c42e995aaf61639a890c9f502d7
Sarah P <lovesarahinc@gmail.com>
2021-07-28 11:35:16 -0400

    feat(breadcrumb): getting the array of links to work

---
+++ src/components/base/outline-breadcrumbs/test/outside-breadcrumbs.test.ts
@@ -0,0 +1,19 @@
+import { OutlineBreadcrumbs } from '../outline-breadcrumbs';
+import { assert, fixture, html } from '@open-wc/testing';
+
+describe('outline-breadcrumbs', () => {
+  it('is defined', () => {
+    const el = document.createElement('outline-breadcrumbs');
+    assert.instanceOf(el, OutlineBreadcrumbs);
+  });
+
+  it('renders with default values', async () => {
+    const el = await fixture(html`<outline-breadcrumbs></outline-breadcrumbs>`);
+    assert.shadowDom.equal(
+      el,
+      `
+      <slot></slot>
+    `
+    );
+  });
+});
