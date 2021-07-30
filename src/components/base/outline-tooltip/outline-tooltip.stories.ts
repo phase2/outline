8a1a42f9a6ba8ce2b225d42606d9c1da216a78ad
IMGoodrich <igoodrich@phase2technology.com>
2021-07-30 11:27:01 -0700

    chore(tooltip): accessibility and style updates

--- src/components/base/outline-tooltip/outline-tooltip.stories.ts
+++ src/components/base/outline-tooltip/outline-tooltip.stories.ts
@@ -2,8 +2,9 @@ import { html, TemplateResult } from 'lit';
 import { ifDefined } from 'lit/directives/if-defined';
 import './outline-tooltip';
 import '../outline-button/outline-button';
+import '../outline-heading/outline-heading';
 export default {
-  title: 'Atoms/OutlineTooltip',
+  title: 'Atoms/Tooltip',
   component: 'outline-tooltip',
   argTypes: {
     id: {
@@ -33,55 +34,56 @@ export default {
 
 const InlineTemplate = ({ id, tip, position }): TemplateResult =>
   html`
-    <div style=${`max-width: 50%`}>
-      <h2 style=${`font-size: 30px; margin-bottom: 1rem`}>
-        Hover over all caps text below.
-      </h2>
-      <p>
-        Donec id justo. Suspendisse non nisl sit amet velit hendrerit rutrum.
-        Sed mollis, eros et ultrices tempus, mauris ipsum aliquam libero.
-        <outline-tooltip
-          id="${ifDefined(id)}"
-          tip="${ifDefined(tip)}"
-          position="${position}"
-        >
-          TOOL-TIP WRAPPED AROUND THIS TEXT.
-        </outline-tooltip>
-        Praesent metus tellus, elementum eu, semper a, adipiscing nec, purus.
-      </p>
-    </div>
+    <outline-grid gap-size="small">
+      <outline-column col-span-default="6">
+        <outline-heading>Hover over all caps text below.</outline-heading>
+
+        <p>
+          Donec id justo. Suspendisse non nisl sit amet velit hendrerit rutrum.
+          Sed mollis, eros et ultrices tempus, mauris ipsum aliquam libero.
+          <outline-tooltip
+            id="${ifDefined(id)}"
+            tip="${ifDefined(tip)}"
+            position="${position}"
+          >
+            TOOL-TIP WRAPPED AROUND THIS TEXT.
+          </outline-tooltip>
+          Praesent metus tellus, elementum eu, semper a, adipiscing nec, purus.
+        </p>
+      </outline-column>
+    </outline-grid>
   `;
 
 const FormTemplate = ({ id, position }): TemplateResult =>
   html`
-    <div>
-      <h2 style=${`font-size: 30px; margin-bottom: 1rem`}>
-        Simulated Disabled Form Button.
-      </h2>
-      <div>
-        <form>
-          <outline-tooltip id="${id}" position="${position}">
-            <span slot="tip-info"> Enter a valid email adress here </span>
-            <input
-              type="text"
-              style=${`border: blue solid 2px`}
-              placeholder="your email address"
-            />
-          </outline-tooltip>
-        </form>
-      </div>
-    </div>
+    <outline-grid>
+      <outline-column col-span-default="6">
+        <outline-heading> Simulated Disabled Form Button. </outline-heading>
+        <div>
+          <form>
+            <outline-tooltip id="${id}" position="${position}">
+              <span slot="tip-info"> Enter a valid email address here </span>
+              <input
+                type="text"
+                style=${`border: blue solid 2px`}
+                placeholder="your email address"
+              />
+            </outline-tooltip>
+          </form>
+        </div>
+      </outline-column>
+    </outline-grid>
   `;
 
-export const InlineTooltip = InlineTemplate.bind({});
-InlineTooltip.args = {
+export const WithTipProp = InlineTemplate.bind({});
+WithTipProp.args = {
   id: 'tooltip-id',
   tip: '<div><h2>Thing of Import</h2><p>Important information to be shared by this tooltip</p></div>',
   position: 'bottom',
 };
 
-export const FormTooltip = FormTemplate.bind({});
-FormTooltip.args = {
+export const WithTipInfoSlot = FormTemplate.bind({});
+WithTipInfoSlot.args = {
   id: 'tooltip-1',
   position: 'top',
 };
--- src/components/base/outline-tooltip/outline-tooltip.ts
+++ src/components/base/outline-tooltip/outline-tooltip.ts
@@ -1,8 +1,14 @@
 import { OutlineElement } from '../outline-element/outline-element';
 import { html, TemplateResult, CSSResultGroup } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import {
+  customElement,
+  property,
+  queryAssignedNodes,
+  query,
+} from 'lit/decorators.js';
 import { unsafeHTML } from 'lit/directives/unsafe-html.js';
 import componentStyles from './outline-tooltip.css.lit';
+import '../outline-button/outline-button';
 
 export type TooltipPosition = 'top' | 'bottom' | 'left' | 'right';
 
@@ -23,11 +29,6 @@ export class OutlineTooltip extends OutlineElement {
   @property({ type: String }) id!: string;
 
   /**
-   * Identifies the component as a tooltip for accessibility technologies.
-   */
-  @property({ type: String, reflect: true }) role = 'tooltip';
-
-  /**
    * An optional prop to pass the tip info. Can also be passed via the
    * "tip-info" slot.
    */
@@ -38,14 +39,80 @@ export class OutlineTooltip extends OutlineElement {
    */
   @property({ type: String }) position: TooltipPosition = 'bottom';
 
+  /**
+   * Ref to element put in the tip-info slot
+   */
+  @queryAssignedNodes('tip-info', true) tipSlot: Array<HTMLSlotElement>;
+
+  /**
+   * Ref to string passed to tip prop.
+   */
+  @query('.tip-info') tipString: HTMLElement;
+
   render(): TemplateResult {
     return html`
-      <div class="tooltip" id=${this.id}>
-        <slot></slot>
-        <div class="tooltip-tip position-${this.position}">
-          <slot name="tip-info"> ${unsafeHTML(`${this.tip}`)} </slot>
+      <div class="tooltip" @mouseover=${this.adjustRect}>
+        <slot aria-describedby=${this.id}></slot>
+        <div
+          class="tooltip-tip position-${this.position}"
+          id=${this.id}
+          role="tooltip"
+        >
+          ${!this.tip
+            ? html`<slot id="tip-info" name="tip-info"></slot>`
+            : html`<div class="tip-info">${unsafeHTML(`${this.tip}`)}</div>`}
+          <outline-button @click="${this.close}">x</outline-button>
         </div>
       </div>
     `;
+  }
+
+  /**
+   * Hides the tooltip.
+   */
+  close() {
+    const ele = this.shadowRoot?.querySelector('.tooltip-tip') as HTMLElement;
+    ele.setAttribute('style', 'opacity: 0;');
+  }
+
+  /**
+   * Checks if the tooltip is visible. If not sets potition to the opposite side.
+   * TODO: Maybe come up with better recursive solution to check all possible positions.
+   */
+  adjustRect() {
+    const rect = this.tipString
+      ? (this.tipString?.getBoundingClientRect() as DOMRect)
+      : (this.tipSlot[0]?.getBoundingClientRect() as DOMRect);
+
+    const isVisable =
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth);
+
+    if (isVisable) {
+      return;
+    }
+    if (!isVisable) {
+      switch (this.position) {
+        case 'top':
+          this.position = 'bottom';
+          this.requestUpdate();
+          break;
+        case 'bottom':
+          this.position = 'top';
+          this.requestUpdate();
+          break;
+        case 'left':
+          this.position = 'right';
+          this.requestUpdate();
+          break;
+        case 'right':
+          this.position = 'left';
+          this.requestUpdate();
+          break;
+      }
+    }
   }
 }
