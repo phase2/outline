77347cab98cf8df05537cfc37cf43a678a546320
IMGoodrich <igoodrich@phase2technology.com>
2021-08-06 14:42:22 -0700

    feat(button-with-tooltip): add builtin tooltip/disabled info functionality to button

--- src/components/base/outline-button/outline-button.stories.ts
+++ src/components/base/outline-button/outline-button.stories.ts
@@ -3,8 +3,9 @@ import { ifDefined } from 'lit/directives/if-defined';
 // import { AllIcons } from '../outline-icon/config';
 import { argTypeTarget } from '../outline-link/config';
 import { argTypeSlotContent } from '../../base/outline-element/utils/utils';
-import { ButtonVariant } from './outline-button';
+import { ButtonVariant, ButtonSize } from './outline-button';
 import './outline-button';
+import { TooltipPosition } from '../outline-tooltip/outline-tooltip';
 
 const buttonOptions: ButtonVariant[] = [
   'none',
@@ -13,26 +14,70 @@ const buttonOptions: ButtonVariant[] = [
   'tertiary',
 ];
 
+const sizeOptions: ButtonSize[] = ['small', 'medium', 'large'];
+
+const tooltipPositionOptions: TooltipPosition[] = [
+  'bottom',
+  'top',
+  'left',
+  'right',
+];
+
 export default {
   title: 'Atoms/Button',
   component: 'outline-button',
   argTypes: {
     ...argTypeSlotContent,
     target: argTypeTarget,
-    disabled: {
+    isDisabled: {
+      description:
+        '**Boolean:** Set whether the button is disabled. Only applies to implementations not using the url property',
       control: {
         type: 'boolean',
       },
     },
+    disabledInfo: {
+      description:
+        '**String:** Sets the text for screen readers and tooltips for disabled buttons. If your button can be disabled you must provide a short explanitory reason.',
+      control: {
+        type: 'text',
+      },
+    },
+    tooltipInfo: {
+      description: '**String:** Sets the text for screen readers and tooltips.',
+      control: {
+        type: 'text',
+      },
+    },
     url: {
+      description:
+        '**String:** The url to use for a link. This will render an anchor element. Do not set this prop if you want to render a button element.',
       control: {
         type: 'text',
       },
     },
     variant: {
+      description:
+        '**ButtonVariant (none, primary, secondary, teritary)**: The button style variant to use.',
       control: {
         type: 'select',
         options: buttonOptions,
+      },
+    },
+    size: {
+      description:
+        '**ButtonSize (small, medium, large)** The button size to use',
+      control: {
+        type: 'select',
+        options: sizeOptions,
+      },
+    },
+    position: {
+      description:
+        '**TooltipPosition (top, bottom, left, right)** Sets the position of the **tooltipInfo** / **disabledInfo** display on hover.',
+      control: {
+        type: 'select',
+        options: tooltipPositionOptions,
       },
     },
     // icon: {
@@ -41,6 +86,14 @@ export default {
     //     options: AllIcons,
     //   },
     // },
+    onClick: {
+      description:
+        'A click handler to be passed only to onClick. **DO NOT USE** `@click` on this component.',
+    },
+    onKeyUp: {
+      description:
+        'A keyUp handler to be passed to the onKeyUp. **DO NOT USE** `@keyup` on this component.',
+    },
   },
   args: {
     variant: 'primary',
@@ -75,19 +128,31 @@ This is rendered as an \`a\` element if a link and a \`button\` element if not a
 
 const Template = ({
   defaultSlot,
-  disabled,
+  isDisabled,
   icon,
   url,
   target,
   variant,
+  size,
+  onClick,
+  onKeyUp,
+  disabledInfo,
+  tooltipInfo,
+  position,
 }): TemplateResult =>
   html`
     <outline-button
       url="${ifDefined(url)}"
       target="${ifDefined(target)}"
       variant="${variant}"
-      disabled="${ifDefined(disabled)}"
+      size="${size}"
+      ?isDisabled="${isDisabled}"
       icon="${ifDefined(icon)}"
+      .onClick="${onClick}"
+      .onKeyUp="${onKeyUp}"
+      .disabledInfo="${disabledInfo}"
+      .tooltipInfo="${tooltipInfo}"
+      position="${position}"
     >
       ${defaultSlot}
     </outline-button>
@@ -97,11 +162,13 @@ export const Link = Template.bind({});
 Link.args = {
   defaultSlot: 'Link button',
   url: '#',
+  size: 'medium',
 };
 
 export const IconLink = Template.bind({});
 IconLink.args = {
   defaultSlot: 'Link button w/icon',
+  size: 'medium',
   url: '#',
   variant: 'primary',
   icon: 'arrowNarrowRightOutline',
@@ -110,17 +177,21 @@ IconLink.args = {
 export const PrimaryButton = Template.bind({});
 PrimaryButton.args = {
   defaultSlot: 'Primary button',
+  size: 'medium',
   variant: 'primary',
+  position: 'bottom',
 };
 
 export const SecondaryButton = Template.bind({});
 SecondaryButton.args = {
   defaultSlot: 'Secondary button',
   variant: 'secondary',
+  size: 'medium',
 };
 
 export const TertiaryButton = Template.bind({});
 TertiaryButton.args = {
   defaultSlot: 'Tertiary button',
   variant: 'tertiary',
+  size: 'medium',
 };
--- src/components/base/outline-button/outline-button.ts
+++ src/components/base/outline-button/outline-button.ts
@@ -3,6 +3,9 @@ import { customElement, property } from 'lit/decorators.js';
 import { LinkTargetType } from '../outline-link/config';
 import componentStyles from './outline-button.css.lit';
 import { OutlineElement } from '../outline-element/outline-element';
+import { ifDefined } from 'lit/directives/if-defined';
+import { TooltipPosition } from '../outline-tooltip/outline-tooltip';
+import '../outline-tooltip/outline-tooltip';
 
 // import {
 //   IconTypeOutline,
@@ -12,9 +15,7 @@ import { OutlineElement } from '../outline-element/outline-element';
 
 export type ButtonVariant = 'none' | 'primary' | 'secondary' | 'tertiary';
 
-export type ButtonSize = 'small' | 'large';
-
-export type ButtonType = 'link' | 'button';
+export type ButtonSize = 'small' | 'medium' | 'large';
 
 /**
  * The Outline Button component
@@ -37,8 +38,12 @@ export class OutlineButton extends OutlineElement {
   /**
    * The button style variant to use.
    */
-  @property({ reflect: true, attribute: 'button-variant' })
-  variant: ButtonVariant = 'primary';
+  @property({ type: String }) variant: ButtonVariant = 'primary';
+
+  /**
+   * The button size to use.
+   */
+  @property({ type: String }) size: ButtonSize = 'medium';
 
   /**
    * Choose which predefined icon to add to the link
@@ -49,19 +54,94 @@ export class OutlineButton extends OutlineElement {
    * Whether the button is disabled. Only applies to
    * implementations not using the url property
    */
-  @property({ type: Boolean }) disabled: boolean;
+  @property({ type: Boolean }) isDisabled = false;
+
+  /**
+   * A click handler to be passed only to onClick. DO NOT USE @click on this component.
+   */
+  @property() onClick: () => void;
+
+  /**
+   * A keyUp handler to be passed to the onKeyUp. DO NOT USE @keyup on this component.
+   */
+  @property() onKeyUp: () => void;
+
+  /**
+   * Sets the text for screen readers and tooltips for disabled buttons.
+   * If your button can be disabled you must provide a short explanitory reason.
+   */
+  @property({ type: String }) disabledInfo?: string | undefined;
+
+  /**
+   * Sets the text for screen readers and tooltips. .
+   */
+  @property({ type: String }) tooltipInfo?: string | undefined;
+
+  /**
+   * Sets the position of the tooltipInfo/disabledInfo display on hover.
+   */
+  @property({ type: String }) position: TooltipPosition | undefined;
 
   render(): TemplateResult {
-    return this.url !== undefined
+    return this.hasTooltip()
+      ? html`
+          <outline-tooltip
+            position="${ifDefined(this.position)}"
+            .tip=${this.setAriaDescription()}
+          >
+            <button
+              class="btn ${this.variant} ${this.size}"
+              aria-disabled="${this.isDisabled}"
+              .onclick="${this.onClick}"
+              .onkeyup="${this.onKeyUp}"
+            >
+              <slot></slot>
+            </button>
+          </outline-tooltip>
+        `
+      : html`${this.basicTemplate()} `;
+  }
+
+  basicTemplate() {
+    return this.url
       ? html` <a
-          class="btn ${this.variant}"
+          class="btn ${this.variant} ${this.size}"
           href=${this.url}
           target=${this.target}
+          aria-disabled=${this.isDisabled}
         >
           <slot></slot>
         </a>`
-      : html` <button class="btn ${this.variant}" .disabled=${this.disabled}>
+      : html`<button
+          class="btn ${this.variant} ${this.size}"
+          aria-disabled="${this.isDisabled}"
+          .onclick="${this.onClick}"
+          .onkeyup="${this.onKeyUp}"
+        >
           <slot></slot>
-        </button>`;
+        </button> `;
+  }
+
+  showDisabled = (): boolean => !!this.isDisabled && !!this.disabledInfo;
+
+  showTipInfo = (): boolean => !!this.tooltipInfo && !this.isDisabled;
+
+  hasTooltip = (): boolean => this.showDisabled() || this.showTipInfo();
+
+  setAriaDescription = () => {
+    if (this.showDisabled()) {
+      return this.disabledInfo;
+    } else if (this.showTipInfo()) {
+      return this.tooltipInfo;
+    } else return;
+  };
+
+  updated() {
+    // checks the isDisabled prop and manages aria-disabled attribues on the <outline-button> element itself.
+    if (this.hasAttribute('isDisabled')) {
+      this.setAttribute('aria-disabled', 'true');
+    } else {
+      this.setAttribute('aria-disabled', 'false');
+    }
   }
 }
