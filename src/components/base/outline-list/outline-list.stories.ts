b9e4d4e25812318c9ee19d5b05a642e80bb0d2d3
IMGoodrich <igoodrich@phase2technology.com>
2021-09-16 13:19:18 -0700

    feat(outline-list): import smc improvemnts: divider, orientations, columns, itemcount slider

--- src/components/base/outline-list/outline-list.stories.ts
+++ src/components/base/outline-list/outline-list.stories.ts
@@ -1,6 +1,12 @@
 import { html, TemplateResult } from 'lit';
 import { ifDefined } from 'lit/directives/if-defined';
 import { unsafeHTML } from 'lit/directives/unsafe-html';
+import {
+  listOrientations,
+  listTypes,
+  columnsCount,
+  dividerColors,
+} from './outline-list';
 import './outline-list';
 import '../outline-heading/outline-heading';
 export default {
@@ -58,20 +64,36 @@ export default {
         '**`<ListType>`("ol" | "ul" | "div"):** <br> Determines which type of list is rendered.',
       control: {
         type: 'select',
-        options: ['ul', 'ol', 'div'],
+        options: listTypes,
       },
       name: 'list-type',
     },
     orientation: {
       description:
         '**`<ListOrientation>`("row" | "column"):** <br> Sets orientation of list',
-      control: { type: 'select', options: ['row', 'column'] },
+      control: { type: 'select', options: listOrientations },
     },
     navLabel: {
       description:
         'If set will wrap list in a nav tag with the passed string set as the aria-label',
       control: { type: 'text' },
       name: 'nav-label',
+    },
+    columns: {
+      description:
+        '**`<ColumnCount>`("2", "3", "4"):** <br> If set, overrides orientation setting and renders list above mobile in selected number of columns',
+      control: {
+        type: 'select',
+        options: [...columnsCount, undefined],
+      },
+    },
+    divider: {
+      description:
+        '**`<ListDividerColors>` ("orange","green","blue","teal","purple","white"):** <br>If set adds a pseudo element divider of chose color between all list items. Currently only setup for `<ul/>`. Best only for row.',
+      control: {
+        type: 'select',
+        options: [...dividerColors, undefined],
+      },
     },
     headingSlotContent: {
       control: { type: 'text' },
@@ -83,36 +105,53 @@ export default {
       name: 'footer slot content',
       description: '**slot="footer"**',
     },
-    items: {
-      description: '**NOT A PROP**',
+    itemCount: {
+      control: { type: 'range', min: 0, max: 30, step: 1 },
     },
   },
   args: {
+    divider: false,
+    columns: false,
+    listType: 'ul',
+    navLabel: '',
+    orientation: ' row',
+    itemCount: 6,
     headingSlotContent: '',
     footerSlotContent: '',
   },
+};
+const items = itemCount => {
+  const itemList = [];
+  for (let i = 0; i < itemCount; i++) {
+    itemList.push({ linkHref: '/', text: `link ${i + 1}` });
+  }
+  return itemList;
 };
 
 const Template = ({
   listType,
   navLabel,
   orientation,
-  items,
+  itemCount,
   headingSlotContent,
   footerSlotContent,
-}): TemplateResult =>
-  html`
+  divider,
+  columns,
+}): TemplateResult => {
+  return html`
     <outline-list
       list-type="${listType}"
       nav-label="${navLabel}"
       orientation="${orientation}"
+      .divider=${divider}
+      .columns=${columns}
     >
       <outline-heading slot="heading" level="h3" levelSize="3xl"
         >${headingSlotContent}</outline-heading
       >
-      ${items.map(
+      ${items(itemCount).map(
         item => html`
-          <li style=${`padding: 1rem; list-style: none`}>
+          <li style=${`padding: 0.5rem; list-style: none`}>
             <outline-link
               linkHref=${item.linkHref}
               linkText=${item.text}
@@ -123,17 +162,6 @@ const Template = ({
       <slot slot="footer">${ifDefined(unsafeHTML(footerSlotContent))}</slot>
     </outline-list>
   `;
+};
 
 export const List = Template.bind({});
-List.args = {
-  listType: 'ul',
-  orientation: 'row',
-  navLabel: 'main nav',
-  items: [
-    { linkHref: '#', text: 'About' },
-    { linkHref: '#', text: 'Contact' },
-    { linkHref: '#', text: 'Jobs' },
-    { linkHref: '#', text: 'News' },
-    { linkHref: '#', text: 'FAQ' },
-  ],
-};
--- src/components/base/outline-list/outline-list.ts
+++ src/components/base/outline-list/outline-list.ts
@@ -3,15 +3,38 @@ import { OutlineElement } from '../outline-element/outline-element';
 import { customElement, property } from 'lit/decorators.js';
 import componentStyles from './outline-list.css.lit';
 import '../outline-link/outline-link';
+import { classMap } from 'lit/directives/class-map.js';
 
-export type ListType = 'ol' | 'ul' | 'div';
+export const listTypes = ['ol', 'ul', 'div'] as const;
 
-export type ListOrientation = 'column' | 'row';
+export type ListType = typeof listTypes[number];
 
+export const listOrientations = [
+  'column',
+  'row',
+  'mobile-row',
+  'mobile-col',
+  'col-center',
+  'mobile-col-center',
+] as const;
+
+export type ListOrientation = typeof listOrientations[number];
+
+export const dividerColors = ['blue', 'teal', 'red', 'white', 'black'] as const;
+
+export type ListDividerColors = typeof dividerColors[number];
+
+export const columnsCount = ['2', '3', '4'] as const;
+
+export type ColumnCount = typeof columnsCount[number];
+
+export type ClassInfo = { [name: string]: string | boolean | number };
 /**
  * The OutlineList component
  * @element outline-list
- * @slot default slot
+ * @slot default slot.
+ * @slot heading: for content above the default slot.
+ * @slot footer: for content below the default slot.
  */
 @customElement('outline-list')
 export class OutlineList extends OutlineElement {
@@ -38,35 +61,60 @@ export class OutlineList extends OutlineElement {
   @property({ type: String, attribute: 'nav-label' })
   navLabel: string | undefined;
 
+  /**
+   * If set adds a pseudo element divider of chose color
+   * between all list items. Best only for row.
+   */
+  @property({ type: String })
+  divider: ListDividerColors;
+
+  /**
+   *  If set, overrides orientation setting and
+   *  renders list above mobile in selected number of columns.
+   */
+  @property({ type: String })
+  columns: ColumnCount;
+
   render(): TemplateResult {
+    const classes = {
+      list: !this.columns,
+      grid: this.columns,
+      [`${this.orientation}`]: this.orientation && !this.columns,
+      divided: this.divider,
+      [`${this.divider}`]: this.divider,
+      [`columns--${this.columns}`]: this.columns,
+    };
+
     return this.navLabel
       ? html`
           <slot name="heading"></slot>
-          <nav aria-label="${this.navLabel}">${this.listCase()}</nav>
+          <nav role="navigation" aria-label="${this.navLabel}">
+            ${this.listCase(classes)}
+          </nav>
           <slot name="footer"></slot>
         `
-      : html`<slot name="header"></slot>
-          ${this.listCase()}
+      : html`<slot name="heading"></slot>
+          ${this.listCase(classes)}
           <slot name="footer"></slot>`;
   }
 
-  listCase() {
+  listCase(classes: ClassInfo) {
     switch (this.listType) {
       case 'ol':
         return html`
-          <ol class="list ${this.orientation}">
+          <ol class=${classMap(classes)}>
             <slot></slot>
           </ol>
         `;
       case 'ul':
         return html`
-          <ul class="list ${this.orientation}">
+          <ul class=${classMap(classes)}>
             <slot></slot>
           </ul>
         `;
       default:
         return html`
-          <div class="list ${this.orientation}">
+          <div class=${classMap(classes)}>
             <slot></slot>
           </div>
         `;
