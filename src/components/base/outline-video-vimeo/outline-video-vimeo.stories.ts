76bd00b733b503e7446e46e56cd380f400963a78
Dan Montgomery <dmontgomery@phase2technology.com>
2021-10-25 16:42:07 -0500

    feat(vimeo-video): add component
    Basic Vimeo video component that fits the available space.

---
+++ src/components/base/outline-video-vimeo/outline-video-vimeo.stories.ts
@@ -0,0 +1,52 @@
+import { html, TemplateResult } from 'lit';
+import './outline-video-vimeo';
+import { ifDefined } from 'lit/directives/if-defined.js';
+
+const configuration = {
+  title: 'Content/Vimeo Video',
+  component: 'outline-video-vimeo',
+  argTypes: {
+    videoID: {
+      name: 'video-id',
+      description: 'Vimeo video id',
+      control: { type: 'text' },
+    },
+  },
+  args: {
+    videoID: `253989945`,
+  },
+  parameters: {
+    docs: {
+      description: {
+        component: `
+Vimeo video. Allows the embedded video to fit the space.
+`,
+      },
+      source: {
+        // This code sample will be used for every example unless overridden.
+        code: `
+<outline-video-vimeo
+  video-id="{{ videoID }}"
+>
+</outline-video-vimeo>
+        `,
+      },
+    },
+  },
+};
+
+export default configuration;
+
+const Template = (args = configuration.args): TemplateResult => {
+  args = {
+    ...configuration.args,
+    ...args,
+  };
+
+  return html`
+    <outline-video-vimeo video-id="${ifDefined(args.videoID)}">
+    </outline-video-vimeo>
+  `;
+};
+
+export const VimeoVideo = Template.bind({});
