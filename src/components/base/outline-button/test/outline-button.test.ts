e90936bed34d66a0d50b6caf91cccf7a5d71d605
Jake Strawn <jake.strawn@gmail.com>
2021-12-11 14:49:19 -0500

    fix(tests): update tests for outline-button

--- src/components/base/outline-button/test/outline-button.test.ts
+++ src/components/base/outline-button/test/outline-button.test.ts
@@ -12,7 +12,80 @@ describe('outline-button', () => {
     assert.shadowDom.equal(
       el,
       `
-      <button class="btn primary">
+      <button aria-disabled="false" class="btn medium primary">
+        <slot></slot>
+      </button>
+    `
+    );
+  });
+
+  it('renders as a link', async () => {
+    const el = await fixture(
+      html`<outline-button
+        url="https://outline.phase2tech.com"
+        target="_blank"
+      ></outline-button>`
+    );
+    assert.shadowDom.equal(
+      el,
+      `
+      <a aria-disabled="false" class="btn medium primary" href="https://outline.phase2tech.com" target="_blank">
+        <slot></slot>
+      </a>
+    `
+    );
+  });
+
+  it('renders as a disabled button', async () => {
+    const el = await fixture(
+      html`<outline-button isDisabled></outline-button>`
+    );
+    assert.shadowDom.equal(
+      el,
+      `
+      <button aria-disabled="true" class="btn medium primary">
+        <slot></slot>
+      </button>
+    `
+    );
+  });
+
+  it('renders a primary button variant', async () => {
+    const el = await fixture(
+      html`<outline-button variant="primary"></outline-button>`
+    );
+    assert.shadowDom.equal(
+      el,
+      `
+      <button aria-disabled="false" class="btn medium primary">
+        <slot></slot>
+      </button>
+    `
+    );
+  });
+
+  it('renders a secondary button variant', async () => {
+    const el = await fixture(
+      html`<outline-button variant="secondary"></outline-button>`
+    );
+    assert.shadowDom.equal(
+      el,
+      `
+      <button aria-disabled="false" class="btn medium secondary">
+        <slot></slot>
+      </button>
+    `
+    );
+  });
+
+  it('renders a tertiary variant', async () => {
+    const el = await fixture(
+      html`<outline-button variant="tertiary"></outline-button>`
+    );
+    assert.shadowDom.equal(
+      el,
+      `
+      <button aria-disabled="false" class="btn medium tertiary">
         <slot></slot>
       </button>
     `
