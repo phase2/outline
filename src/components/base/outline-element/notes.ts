9b56876a49babb53d785fa1149c04bec5fa9e4de
Jake Strawn <jake.strawn@gmail.com>
2021-12-20 11:12:48 -0500

    fix: remove unitentional file

--- src/components/base/outline-element/notes.ts
+++
@@ -1,5 +0,0 @@
-/**
- * @file - Entry point for Outline to incorporate Lazy Loaded components.
- * @see https://lamplightdev.com/blog/2020/03/20/lazy-loading-web-components-with-intersection-observer/
- * @see https://github.com/lit/lit/tree/main/packages/labs/observers
- */
