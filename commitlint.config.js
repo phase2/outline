4d3833149335d0a5448f9dd871ae8cadbeb36f60
Jake Strawn <jake.strawn@gmail.com>
2022-01-12 16:59:44 -0500

    feat(commitlint): ensure subject-case and subject-full-stop are optional

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,11 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'body-max-line-length': [0, 'always'], // Make sure there is never a max-line-length by disabling the rule
-    'subject-case': [1, 'always', 'sentence-case'], // @see https://github.com/conventional-changelog/commitlint/tree/master/%40commitlint/config-conventional#subject-case
-    'subject-full-stop': [1, 'always', '.'], // @see https://github.com/conventional-changelog/commitlint/tree/master/%40commitlint/config-conventional#subject-full-stop
+    // Make sure there is never a max-line-length by disabling the rule
+    'body-max-line-length': [0, 'always'],
+    // @see https://github.com/conventional-changelog/commitlint/tree/master/%40commitlint/config-conventional#subject-case
+    'subject-case': [1, 'always', 'sentence-case'],
+    // @see https://github.com/conventional-changelog/commitlint/tree/master/%40commitlint/config-conventional#subject-full-stop
+    'subject-full-stop': [1, 'always', '.'],
   },
 };
