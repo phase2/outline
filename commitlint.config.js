e41657f49ae239ff74f22d8ce25c46a7a70101a9
Jake Strawn <jake.strawn@gmail.com>
2022-01-12 16:59:09 -0500

    feat(commitlint): Updates to commit configuraiton.

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,5 +2,7 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'body-max-line-length': [0, 'always'], // Make sure there is never a max-line-length by disabling the rule
+    'subject-case': [1, 'always', 'sentence-case'], // @see https://github.com/conventional-changelog/commitlint/tree/master/%40commitlint/config-conventional#subject-case
+    'subject-full-stop': [1, 'always', '.'], // @see https://github.com/conventional-changelog/commitlint/tree/master/%40commitlint/config-conventional#subject-full-stop
   },
 };
