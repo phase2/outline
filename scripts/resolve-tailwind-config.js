aeb592ccf53b056da5abeb65903946320cda6e6e
Dan Montgomery <dmontgomery@phase2technology.com>
2022-01-12 16:10:10 -0600

    feat: remove prettier
    `yarn remove prettier eslint-config-prettier`

--- scripts/resolve-tailwind-config.js
+++ scripts/resolve-tailwind-config.js
@@ -1,41 +1,29 @@
 #! usr/bin/env node
 const fs = require('fs');
 const path = require('path');
-const { format, resolveConfig: resolvePrettierConfig } = require('prettier');
 const resolveTailwindConfig = require('tailwindcss/resolveConfig');
-const flattenColorPalette =
-  require('tailwindcss/lib/util/flattenColorPalette').default;
+const flattenColorPalette = require('tailwindcss/lib/util/flattenColorPalette').default;
 
 const tailwindConfig = require('../tailwind.config');
 
 const tailwindThemeConfig = resolveTailwindConfig(tailwindConfig).theme;
 
-const flattenTailwindColors = colors =>
-  Object.keys(flattenColorPalette(colors));
+const flattenTailwindColors = colors => Object.keys(flattenColorPalette(colors));
 
 const createTailwindUnionType = colors => colors.map(v => `'${v}'`).join(' | ');
 
-resolvePrettierConfig('.prettierrc')
-  .then(options => {
-    const tailwindColors = flattenTailwindColors(tailwindThemeConfig.colors);
-    return format(
-      `
-  /**
-   * This file is auto generated via npm run generate.resolved-tailwind
-   */
+const tailwindColors = flattenTailwindColors(tailwindThemeConfig.colors);
 
-  export type Color = ${createTailwindUnionType(tailwindColors)}
+const formatted = `
+/**
+ * This file is auto generated via npm run generate.resolved-tailwind
+ */
 
-  export const ALL_COLORS: Color[] = ${JSON.stringify(tailwindColors)}
+export type Color = ${createTailwindUnionType(tailwindColors)}
 
-  export default ${JSON.stringify(tailwindThemeConfig)}
-  `,
-      { ...options, parser: 'typescript' }
-    );
-  })
-  .then(formatted =>
-    fs.writeFileSync(
-      path.join(__dirname, '../src/resolved-tailwind-config.ts'),
-      formatted
-    )
-  );
+export const ALL_COLORS: Color[] = ${JSON.stringify(tailwindColors)}
+
+export default ${JSON.stringify(tailwindThemeConfig)}
+`;
+
+fs.writeFileSync(path.join(__dirname, '../src/resolved-tailwind-config.ts'),formatted);
