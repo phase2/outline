2818ada69fbcfe3c9777486014b17a5baa8a1ea8
IMGoodrich <igoodrich@phase2technology.com>
2022-03-03 14:02:46 -0800

    feat(outline-nav): Add outline-nav component, stories, and styles.

---
+++ src/components/base/outline-nav-menu/outline-nav/outline-nav.stories.ts
@@ -0,0 +1,34 @@
+import { html, TemplateResult } from 'lit';
+import { ifDefined } from 'lit/directives/if-defined';
+import './outline-nav';
+import '../outline-nav-item/outline-nav-item';
+import '../outline-nav-link/outline-nav-link';
+import testData from '../nav-test-data';
+export default {
+  title: 'Navigation/OutlineNav',
+  component: 'outline-nav',
+  argTypes: {},
+  args: {
+    items: testData
+  },
+};
+
+const Template = ({items}): TemplateResult =>
+  html`
+    <outline-nav .listItems=${items}> </outline-nav>
+  `;
+
+const SlotsTemplate = (): TemplateResult =>
+  html`
+    <outline-nav> 
+      <outline-nav-item .item=${{text:"Item 1", url:"#", sub:[]}} slot="list"></outline-nav-item>
+      <outline-nav-item .item=${{text:"Item 2", url:"#", sub:[]}} slot="list"></outline-nav-item>
+      <outline-nav-item .item=${{ text: "Item 3", url: "#", sub:[] }} slot="list"></outline-nav-item>
+      <outline-nav-link .item=${{ text: "nav link 1", url: "#" }} slot="list"> </outline-nav-link>
+      <outline-nav-link slot="list"><a href="#">Slotted</a></outline-nav-link>
+    </outline-nav>
+  `;
+
+export const OutlineNavWithData = Template.bind({});
+
+export const OutlineNavWithSlots = SlotsTemplate.bind({});
---
+++ src/components/base/outline-nav-menu/outline-nav/outline-nav.ts
@@ -0,0 +1,104 @@
+import { CSSResultGroup, TemplateResult, html } from 'lit';
+import { OutlineElement } from '../../outline-element/outline-element';
+import { customElement, property, queryAssignedNodes } from 'lit/decorators.js';
+import componentStyles from './outline-nav.css.lit';
+import { MobileController } from '../../../controllers/mobile-controller';
+
+import '../outline-nav-link/outline-nav-link';
+import '../outline-nav-item/outline-nav-item';
+
+export type MenuLink = {
+  text: string;
+  url: string;
+};
+
+export type MenuNavItem = {
+  text: string;
+  sub: MenuLink[] | MenuNavItem[];
+  url?: string;
+};
+
+export type MainNavData = {
+  mainNav: MenuNavItem[];
+};
+
+/**
+ * The OutlineNavMenu component
+ * @element outline-nav
+ */
+@customElement('outline-nav')
+export class OutlineNav extends OutlineElement {
+  private mobileController = new MobileController(this);
+  private _seed = Math.floor(Math.random() * 100000).toString();
+  static styles: CSSResultGroup = [componentStyles];
+
+  @property({ type: String })
+  title: string;
+
+  @queryAssignedNodes('list', true, '[slot="list"]')
+  slottedListItems: Array<HTMLElement>;
+
+  @property({ type: Array })
+  listItems: MenuNavItem[];
+
+  @property({ type: String, reflect: true })
+  id = `menu-${this._seed}`;
+
+  firstUpdated() {
+    this.setTopLevelAttribute();
+  }
+
+  render(): TemplateResult {
+    const isMobile = this.mobileController.isMobile;
+
+    return html`<div class="outline-nav">
+      ${this.title
+        ? () => html`<header class="header">
+            <slot name="header"></slot>
+          </header>`
+        : null}
+      <nav aria-label="" class="nav--main">
+        <ul
+          class="outline-nav nav--main-list ${isMobile ? 'mobile' : ''}"
+          role="menu"
+          id="ping"
+        >
+          <slot class="nav--main-list-slot" name="list">
+            ${this.generateListItemsFromData()}
+          </slot>
+        </ul>
+      </nav>
+    </div>`;
+  }
+
+  setTopLevelAttribute() {
+    if (!this.listItems) {
+      [...this.slottedListItems].map(el => {
+        const isNavItem = Object?.keys(el)?.includes('sub') ? true : false;
+        isNavItem ? el.setAttribute('toplevel', 'toplevel') : null;
+        // el.setAttribute('parentID', this.id)
+      });
+    }
+  }
+
+  generateListItemsFromData() {
+    return this.listItems?.map((listItem: MenuLink | MenuNavItem) => {
+      const hasSubList = Object?.keys(listItem)?.includes('sub') ? true : false;
+      if (hasSubList) {
+        const item = listItem as MenuNavItem;
+        return html`<outline-nav-item
+          toplevel
+          .parentID=${this.id}
+          .item=${item}
+          slot="list"
+        ></outline-nav-item>`;
+      } else {
+        const item = listItem as MenuLink;
+        return html`<outline-nav-link
+          .item=${item}
+          slot="list"
+        ></outline-nav-link>`;
+      }
+    });
+  }
+}
