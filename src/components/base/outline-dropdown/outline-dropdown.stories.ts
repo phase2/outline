bdf2427f158d7c53854892e6af87e264a31166d5
Jake Strawn <jake.strawn@gmail.com>
2022-03-09 16:58:54 -0500

    feat(outline-dropdown): Secondary pass at more solid dropdown structure for a11y security.

--- src/components/base/outline-dropdown/outline-dropdown.stories.ts
+++ src/components/base/outline-dropdown/outline-dropdown.stories.ts
@@ -1,12 +1,16 @@
 import { html, TemplateResult } from 'lit';
+import { ifDefined } from 'lit/directives/if-defined.js';
 import { unsafeHTML } from 'lit/directives/unsafe-html.js';
+import { ButtonSize, ButtonVariant } from '../outline-button/outline-button';
 import './outline-dropdown.ts';
+
+const triggerVariants: ButtonVariant[] = ['none', 'primary', 'secondary'];
+const triggerSizes: ButtonSize[] = ['small', 'medium', 'large'];
 
 const configuration = {
   title: 'Navigation/Outline Dropdown',
   component: 'outline-dropdown',
   argTypes: {
-    
     isDisabled: {
       name: 'is-disabled',
       table: { category: 'Properties', defaultValue: { summary: 'false' } },
@@ -25,15 +29,49 @@ const configuration = {
         type: 'boolean',
       },
     },
-    triggerSlot: {
-      name: 'trigger',
-      description: 'The trigger/button slot',
+    url: {
+      name: 'trigger-url',
+      table: { category: 'Properties', defaultValue: { summary: 'NULL' } },
+      description:
+        'The url to use for a link. This will render an anchor element. Do not set this prop if you want to render a button element.',
       control: {
         type: 'text',
       },
-      table: {
-        category: 'Slots',
-        defaultValue: { summary: 'NULL' },
+    },
+    label: {
+      name: 'trigger-label',
+      table: { category: 'Properties', defaultValue: { summary: 'NULL' } },
+      description:
+        'Label to be passed to `aria-label` on the link or button element. This is required for accessibility if we use a button with an icon only.',
+      control: {
+        type: 'text',
+      },
+    },
+    text: {
+      name: 'trigger-text',
+      table: { category: 'Properties', defaultValue: { summary: 'NULL' } },
+      description:
+        'Text of the actual button/link of the trigger element.',
+      control: {
+        type: 'text',
+      },
+    },
+    variant: {
+      name: 'trigger-variant',
+      table: { category: 'Properties', defaultValue: { summary: 'primary' } },
+      description: 'The button style variant to use.',
+      options: triggerVariants,
+      control: {
+        type: 'select',
+      },
+    },
+    size: {
+      name: 'trigger-size',
+      table: { category: 'Properties', defaultValue: { summary: 'medium' } },
+      description: 'The button size variant.',
+      options: triggerSizes,
+      control: {
+        type: 'select',
       },
     },
     dropdownSlot: {
@@ -47,66 +85,76 @@ const configuration = {
         defaultValue: { summary: 'NULL' },
       },
     },
-    footerSlot: {
-      name: 'footer',
-      description: 'The footer slot for the download size.',
-      control: {
-        type: 'text',
-      },
-      table: {
-        category: 'Slots',
-        defaultValue: { summary: 'NULL' },
-      },
-    },
   },
   args: {
-    triggerSlot: `Button Trigger`,
-    dropdownSlot: `<p>This is the content of the dropdown. While this is a simple <code>p</code> tag, any content can be placed in this dropdown using <code>slot="dropdown"</code> on your top level element in the dropdown panel.</p>
+    dropdownSlot: `
+<p>This is the content of the dropdown. This is a pretend <outline-link link-href="#">link</outline-link> so we can tab in. While this is a simple <code>p</code> tag, any content can be placed in this dropdown using <code>slot="dropdown"</code> on your top level element in the dropdown panel.</p>
 <p>This dropdown panel can be styled as needed for any menus that need to extend the <code>outline-dropdown</code> component.</p>
 `,
-    footerSlot: ``,
     isDisabled: false,
     isOpen: false,
+    text: 'Dropdown',
+    variant: 'primary',
+    size: 'medium',
+    label: '',
+    url: 'https://outline.phase2tech.com/',
   },
 };
 
 export default configuration;
 
-const ButtonTemplate = (customArguments = {}): TemplateResult => {
+const DropdownTemplate = (customArguments = {}): TemplateResult => {
   const args = {
     ...configuration.args,
     ...customArguments,
   };
 
-  return html`<outline-dropdown ?is-disabled=${args.isDisabled} ?is-open=${args.isOpen}>
-    <outline-button slot="trigger">${unsafeHTML(
-      args.triggerSlot ?? ''
-    )}</outline-button>
-    <div slot="dropdown">${unsafeHTML(args.dropdownSlot ?? '')}</div>
-    ${unsafeHTML(args.footerSlot ?? '')}
-  </outline-dropdown>`;
+  return html`
+<outline-dropdown 
+  ?is-disabled=${args.isDisabled} 
+  ?is-open=${args.isOpen} 
+  trigger-text="${ifDefined(args.text)}"
+  trigger-label="${ifDefined(args.label)}"
+  trigger-variant="${ifDefined(args.variant)}"
+  trigger-size="${ifDefined(args.size)}"
+  trigger-url="${ifDefined(args.url)}"
+  class="my-4"
+>  
+  <div slot="dropdown">${unsafeHTML(args.dropdownSlot ?? '')}</div>
+</outline-dropdown>`;
 };
 
-export const ButtonTrigger = (customArguments = {}) =>
-  ButtonTemplate(customArguments);
-
-const LinkTemplate = (customArguments = {}): TemplateResult => {
-  const args = {
-    ...configuration.args,
-    ...customArguments,
-  };
-
-  return html`<outline-dropdown ?is-disabled=${args.isDisabled} ?is-open=${args.isOpen}>
-    <outline-link slot="trigger" link-href="#">${unsafeHTML(
-      args.triggerSlot ?? ''
-    )}</outline-link>
-    <div slot="dropdown">${unsafeHTML(args.dropdownSlot ?? '')}</div>
-    ${unsafeHTML(args.footerSlot ?? '')}
-  </outline-dropdown>`;
+export const DropdownTrigger = (customArguments = {}) =>
+DropdownTemplate(customArguments);
+DropdownTrigger.args = {
+  isOpen: false,
 };
 
-export const LinkTrigger = (customArguments = {}) =>
-  LinkTemplate(customArguments);
-LinkTrigger.args = {
-  triggerSlot: `Link Trigger`,
+const MultipleDropdownsTemplate = (): TemplateResult => {
+
+  return html`
+<div class="flex flex-row flex-wrap">
+  <outline-dropdown class="my-4 pr-2" trigger-text="Dropdown" trigger-variant="none" trigger-size="medium" trigger-url="https://outline.phase2tech.com/">  
+    <div slot="dropdown">
+      <p>This is the content of the dropdown. This is a pretend <outline-link link-href="#">link</outline-link> so we can tab in. While this is a simple <code>p</code> tag, any content can be placed in this dropdown using <code>slot="dropdown"</code> on your top level element in the dropdown panel.</p>
+      <p>This dropdown panel can be styled as needed for any menus that need to extend the <code>outline-dropdown</code> component.</p>
+    </div>
+  </outline-dropdown>
+  <outline-dropdown class="my-4 pr-2" trigger-text="Dropdown" trigger-variant="none" trigger-size="medium" trigger-url="https://outline.phase2tech.com/">  
+    <div slot="dropdown">
+      <p>This is the content of the dropdown. This is a pretend <outline-link link-href="#">link</outline-link> so we can tab in. While this is a simple <code>p</code> tag, any content can be placed in this dropdown using <code>slot="dropdown"</code> on your top level element in the dropdown panel.</p>
+      <p>This dropdown panel can be styled as needed for any menus that need to extend the <code>outline-dropdown</code> component.</p>
+    </div>
+  </outline-dropdown>
+  <outline-dropdown class="my-4" trigger-text="Dropdown" trigger-variant="none" trigger-size="medium">  
+    <div slot="dropdown">
+      <p>This is the content of the dropdown. This is a pretend <outline-link link-href="#">link</outline-link> so we can tab in. While this is a simple <code>p</code> tag, any content can be placed in this dropdown using <code>slot="dropdown"</code> on your top level element in the dropdown panel.</p>
+      <p>This dropdown panel can be styled as needed for any menus that need to extend the <code>outline-dropdown</code> component.</p>
+    </div>
+  </outline-dropdown>
+</div>
+`;
 };
+
+export const MultipleDropdowns = () =>
+MultipleDropdownsTemplate();
--- src/components/base/outline-dropdown/outline-dropdown.ts
+++ src/components/base/outline-dropdown/outline-dropdown.ts
@@ -1,4 +1,4 @@
-import { html, CSSResultGroup } from 'lit';
+import { html, CSSResultGroup, TemplateResult } from 'lit';
 import { customElement, property, query } from 'lit/decorators.js';
 import { classMap } from 'lit/directives/class-map.js';
 import { emit, waitForEvent } from '../../../internal/event';
@@ -6,10 +6,13 @@ import { watch } from '../../../internal/watch';
 import componentStyles from './outline-dropdown.css.lit';
 import { OutlineElement } from '../../base/outline-element/outline-element';
 import '../outline-button/outline-button';
+import '../outline-icon/outline-icon';
 import { SlotController } from '../../controllers/slot-controller';
+import { LinkTargetType } from '../outline-link/config';
+import { ButtonVariant } from '../outline-button/outline-button';
+import { ifDefined } from 'lit/directives/if-defined.js';
 
 /**
- * @slot trigger - The dropdown's trigger, usually a `<outline-button>` element.
  * @slot - The dropdown's content.
  * @event outline-show - Emitted when the dropdown opens.
  * @event outline-after-show - Emitted after the dropdown opens and all animations are complete.
@@ -28,7 +31,9 @@ export default class OutlineDropdown extends OutlineElement {
     true // To shift or not to shift LightDom nodes to ShadowDOM.
   );
 
+  @query('.dropdown__trigger') trigger: HTMLElement;
   @query('.dropdown__panel') panel: HTMLElement;
+  @query('.dropdown__positioner') positioner: HTMLElement;
 
   /**
    * Indicates whether or not the dropdown is open.
@@ -37,9 +42,66 @@ export default class OutlineDropdown extends OutlineElement {
   @property({ type: Boolean, reflect: true, attribute: 'is-open' })
   isOpen = false;
 
-  /** Disables the dropdown so the panel will not open. */
+  /**
+   * Disables the dropdown so the panel will not open.
+   */
   @property({ type: Boolean, attribute: 'is-disabled' })
   isDisabled = false;
+
+  /**
+   * Determines if the dropdown trigger is a link or not.
+   */
+  @property({ type: Boolean, attribute: false })
+  isLink = false;
+
+  /**
+   * The dropdown will close when the user interacts outside of this element (e.g. clicking).
+   */
+  @property({ attribute: false })
+  containingElement?: HTMLElement;
+
+  /**
+   * The url to use for a link. This will render an anchor element.
+   * Do not set this prop if you want to render a button element.
+   */
+  @property({ type: String, attribute: 'trigger-url' })
+  triggerUrl: string;
+
+  /**
+   * Visible text for the button/link of the trigger element.
+   */
+  @property({ type: String, attribute: 'trigger-text' })
+  triggerText: string;
+
+  /**
+   * ARIA label attribute to pass down to the resulting button or a
+   * element. This is required for accessibility if we use a button
+   * with an icon only.
+   */
+  @property({ type: String, attribute: 'trigger-label' })
+  triggerLabel: string;
+
+  /**
+   * The target to use for a link, used in conjunction with the url attribute.
+   */
+  @property({ type: String, attribute: 'trigger-target' })
+  triggerTarget: LinkTargetType;
+
+  /**
+   * The button style variant to use.
+   */
+  @property({ type: String, attribute: 'trigger-variant' })
+  triggerVariant: ButtonVariant = 'none';
+
+  connectedCallback() {
+    super.connectedCallback();
+
+    this.handleDocumentKeyDown = this.handleDocumentKeyDown.bind(this);
+
+    if (!this.containingElement) {
+      this.containingElement = this;
+    }
+  }
 
   firstUpdated() {
     this.panel.hidden = !this.isOpen;
@@ -50,9 +112,11 @@ export default class OutlineDropdown extends OutlineElement {
     this.hide();
   }
 
-  /** Shows the dropdown panel. */
+  /**
+   * Shows the dropdown panel.
+   */
   async show() {
-    if (this.isOpen || this.isDisabled) {
+    if (this.isOpen) {
       return undefined;
     }
     this.panel.hidden = false;
@@ -60,7 +124,9 @@ export default class OutlineDropdown extends OutlineElement {
     return waitForEvent(this, 'outline-after-show');
   }
 
-  // /** Hides the dropdown panel */
+  /**
+   * Hides the dropdown panel
+   */
   async hide() {
     if (!this.isOpen) {
       return undefined;
@@ -77,13 +143,68 @@ export default class OutlineDropdown extends OutlineElement {
       return;
     }
     if (this.isOpen) {
-      // Show
+      // Show the panel.
       emit(this, 'outline-show');
       emit(this, 'outline-after-show');
     } else {
-      // Hide
+      // Hide the panel.
       emit(this, 'outline-hide');
       emit(this, 'outline-after-hide');
+    }
+  }
+
+  /**
+   * Returns the focus to the trigger element.
+   *
+   * @todo May need to adjust this to handle focusing on the button OR the icon as the actual trigger..
+   */
+  focusOnTrigger() {
+    const slot = this.trigger.querySelector('slot')!;
+    const trigger = slot.assignedElements({ flatten: true })[0] as
+      | HTMLElement
+      | undefined;
+    if (typeof trigger?.focus === 'function') {
+      trigger.focus();
+    }
+  }
+
+  handleDocumentKeyDown(event: KeyboardEvent) {
+    // Close when escape is pressed.
+    if (event.key === 'Escape') {
+      this.hide();
+      this.focusOnTrigger();
+      return;
+    }
+
+    // Handle tabbing
+    if (event.key === 'Tab') {
+      // Tabbing within an open menu should close the dropdown and refocus the trigger
+      if (this.isOpen) {
+        event.preventDefault();
+        this.hide();
+        this.focusOnTrigger();
+        return;
+      }
+
+      // Tabbing outside of the containing element closes the panel
+      //
+      // If the dropdown is used within a shadow DOM, we need to obtain the activeElement within that shadowRoot,
+      // otherwise `document.activeElement` will only return the name of the parent shadow DOM element.
+      setTimeout(() => {
+        const activeElement =
+          this.containingElement?.getRootNode() instanceof ShadowRoot
+            ? document.activeElement?.shadowRoot?.activeElement
+            : document.activeElement;
+
+        if (
+          !this.containingElement ||
+          activeElement?.closest(
+            this.containingElement.tagName.toLowerCase()
+          ) !== this.containingElement
+        ) {
+          this.hide();
+        }
+      });
     }
   }
 
@@ -96,7 +217,7 @@ export default class OutlineDropdown extends OutlineElement {
     }, 0);
   }
 
-  render() {
+  render(): TemplateResult {
     return html`
       <div
         id="dropdown"
@@ -109,19 +230,53 @@ export default class OutlineDropdown extends OutlineElement {
           class="dropdown__trigger"
           @mouseenter="${this.show}"
           @mouseleave="${this.hide}"
-          @keydown="${this.handleTriggerKeyUp}"
+          @keyup="${this.handleTriggerKeyUp}"
         >
-          <slot name="trigger"></slot>
+          <outline-button
+            button-variant="${ifDefined(this.triggerVariant)}"
+            button-target="${ifDefined(this.triggerTarget)}"
+            button-url="${ifDefined(this.triggerUrl)}"
+            button-label="${ifDefined(this.triggerLabel)}"
+            @click="${this.show}"
+          >
+            ${this.triggerText} ${this.iconTemplate()}
+          </outline-button>
           <div
             class="dropdown__panel"
             aria-hidden=${this.isOpen ? 'false' : 'true'}
             aria-labelledby="dropdown"
           >
             <slot name="dropdown"></slot>
-            <slot name="footer"></slot>
           </div>
         </span>
       </div>
     `;
+  }
+
+  /**
+   * Template partial for the CTA slot.
+   * @param classes
+   * @returns TemplateResult | null
+   */
+  iconTemplate(): TemplateResult | null {
+    if (!this.triggerUrl) return null;
+
+    return html`
+      <outline-icon
+        slot="right"
+        name="chevron-down"
+        library="system"
+        size="1em"
+        label="${ifDefined(this.triggerUrl)}"
+        @keydown="${this.show}"
+        tabindex="${this.triggerUrl ? '0' : '-1'}"
+      ></outline-icon>
+    `;
+  }
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'outline-dropdown': OutlineDropdown;
   }
 }
