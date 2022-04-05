13dfd406cdff1d81176357c01be93f0bfe5cc3ce
IMGoodrich <igoodrich@phase2technology.com>
2022-04-04 17:14:14 -0700

    feat(accordion): Add basic tests, and updates stories.

--- src/components/base/outline-accordion/outline-accordion.stories.ts
+++ src/components/base/outline-accordion/outline-accordion.stories.ts
@@ -12,35 +12,35 @@ export default {
         code: `
   <outline-accordion label=label clean=clean single-panel=singlePanel>
     <outline-accordion-panel slot="panels">
-      <h5 slot="heading">Heading 1</h5>
+      <span slot="heading">Heading 1</span>
       <div class="wysiwyg">
         <h6>Etiam ut purus mattis mauris</h6>
         <p>Suspendisse eu ligula. Proin pretium, leo ac pellentesque mollis, felis nunc ultrices eros, sed gravida augue augue mollis justo. Maecenas ullamcorper, dui et placerat feugiat, eros pede varius nisi, condimentum viverra felis nunc et lorem. Nam at tortor in tellus interdum sagittis. Morbi ac felis. Etiam ultricies nisi vel augue. Praesent venenatis metus at tortor pulvinar varius. Sed cursus turpis vitae tortor. Donec elit libero, sodales nec, volutpat a, suscipit non, turpis. Fusce vulputate eleifend sapien.
         </p>
       </div>
     </outline-accordion-panel>
-      <h5 slot="heading">Heading 2</h5>
+      <span slot="heading">Heading 2</span>
       <div class="wysiwyg">
         <h6>Etiam ut purus mattis mauris</h6>
         <p>Suspendisse eu ligula. Proin pretium, leo ac pellentesque mollis, felis nunc ultrices eros, sed gravida augue augue mollis justo. Maecenas ullamcorper, dui et placerat feugiat, eros pede varius nisi, condimentum viverra felis nunc et lorem. Nam at tortor in tellus interdum sagittis. Morbi ac felis. Etiam ultricies nisi vel augue. Praesent venenatis metus at tortor pulvinar varius. Sed cursus turpis vitae tortor. Donec elit libero, sodales nec, volutpat a, suscipit non, turpis. Fusce vulputate eleifend sapien.
         </p>
       </div>
     </outline-accordion-panel>
-      <h5 slot="heading">Heading 3</h5>
+      <span slot="heading">Heading 3</span>
       <div class="wysiwyg">
         <h6>Etiam ut purus mattis mauris</h6>
         <p>Suspendisse eu ligula. Proin pretium, leo ac pellentesque mollis, felis nunc ultrices eros, sed gravida augue augue mollis justo. Maecenas ullamcorper, dui et placerat feugiat, eros pede varius nisi, condimentum viverra felis nunc et lorem. Nam at tortor in tellus interdum sagittis. Morbi ac felis. Etiam ultricies nisi vel augue. Praesent venenatis metus at tortor pulvinar varius. Sed cursus turpis vitae tortor. Donec elit libero, sodales nec, volutpat a, suscipit non, turpis. Fusce vulputate eleifend sapien.
         </p>
       </div>
     </outline-accordion-panel>
-      <h5 slot="heading">Heading 4</h5>
+      <span slot="heading">Heading 4</span>
       <div class="wysiwyg">
         <h6>Etiam ut purus mattis mauris</h6>
         <p>Suspendisse eu ligula. Proin pretium, leo ac pellentesque mollis, felis nunc ultrices eros, sed gravida augue augue mollis justo. Maecenas ullamcorper, dui et placerat feugiat, eros pede varius nisi, condimentum viverra felis nunc et lorem. Nam at tortor in tellus interdum sagittis. Morbi ac felis. Etiam ultricies nisi vel augue. Praesent venenatis metus at tortor pulvinar varius. Sed cursus turpis vitae tortor. Donec elit libero, sodales nec, volutpat a, suscipit non, turpis. Fusce vulputate eleifend sapien.
         </p>
       </div>
     </outline-accordion-panel>
-      <h5 slot="heading">Heading 5</h5>
+      <span slot="heading">Heading 5</span>
       <div class="wysiwyg">
         <h6>Etiam ut purus mattis mauris</h6>
         <p>Suspendisse eu ligula. Proin pretium, leo ac pellentesque mollis, felis nunc ultrices eros, sed gravida augue augue mollis justo. Maecenas ullamcorper, dui et placerat feugiat, eros pede varius nisi, condimentum viverra felis nunc et lorem. Nam at tortor in tellus interdum sagittis. Morbi ac felis. Etiam ultricies nisi vel augue. Praesent venenatis metus at tortor pulvinar varius. Sed cursus turpis vitae tortor. Donec elit libero, sodales nec, volutpat a, suscipit non, turpis. Fusce vulputate eleifend sapien.
@@ -93,7 +93,7 @@ export default {
     label: 'Frequently Asked Questions',
     PanelsSlotContent: `
     <outline-accordion-panel slot="panels">
-    <h5 slot="heading">Heading 1</h5>
+    <span slot="heading">Heading 1</span>
     <div class='wysiwyg'>
       <h6>Etiam ut purus mattis mauris</h6>
       <p>Suspendisse eu ligula. Proin pretium, leo ac pellentesque mollis, felis nunc ultrices eros, sed gravida augue augue mollis justo. Maecenas ullamcorper, dui et placerat feugiat, eros pede varius nisi, condimentum viverra felis nunc et lorem. Nam at tortor in tellus interdum sagittis. Morbi ac felis. Etiam ultricies nisi vel augue. Praesent venenatis metus at tortor pulvinar varius. Sed cursus turpis vitae tortor. Donec elit libero, sodales nec, volutpat a, suscipit non, turpis. Fusce vulputate eleifend sapien.</p>
@@ -101,7 +101,7 @@ export default {
     </outline-accordion-panel>
 
 <outline-accordion-panel slot="panels">
-    <h5 slot="heading">Heading 2</h5>
+    <span slot="heading">Heading 2</span>
     <div class="wysiwyg">
       <h6>Etiam ut purus mattis mauris</h6>
       <p>Suspendisse eu ligula. Proin pretium, leo ac pellentesque mollis, felis nunc ultrices eros, sed gravida augue augue mollis justo. Maecenas ullamcorper, dui et placerat feugiat, eros pede varius nisi, condimentum viverra felis nunc et lorem. Nam at tortor in tellus interdum sagittis. Morbi ac felis. Etiam ultricies nisi vel augue. Praesent venenatis metus at tortor pulvinar varius. Sed cursus turpis vitae tortor. Donec elit libero, sodales nec, volutpat a, suscipit non, turpis. Fusce vulputate eleifend sapien.</p>
@@ -109,7 +109,7 @@ export default {
 </outline-accordion-panel>
 
 <outline-accordion-panel slot="panels">
-    <h5 slot="heading">Heading 3</h5>
+    <span slot="heading">Heading 3</span>
     <div class="wysiwyg">
       <h6>Etiam ut purus mattis mauris</h6>
       <p>Suspendisse eu ligula. Proin pretium, leo ac pellentesque mollis, felis nunc ultrices eros, sed gravida augue augue mollis justo. Maecenas ullamcorper, dui et placerat feugiat, eros pede varius nisi, condimentum viverra felis nunc et lorem. Nam at tortor in tellus interdum sagittis. Morbi ac felis. Etiam ultricies nisi vel augue. Praesent venenatis metus at tortor pulvinar varius. Sed cursus turpis vitae tortor. Donec elit libero, sodales nec, volutpat a, suscipit non, turpis. Fusce vulputate eleifend sapien.</p>
@@ -117,7 +117,7 @@ export default {
 </outline-accordion-panel>
 
 <outline-accordion-panel slot="panels">
-    <h5 slot="heading">Heading 4</h5>
+    <span slot="heading">Heading 4</span>
     <div class="wysiwyg">
       <h6>Etiam ut purus mattis mauris</h6>
       <p>Suspendisse eu ligula. Proin pretium, leo ac pellentesque mollis, felis nunc ultrices eros, sed gravida augue augue mollis justo. Maecenas ullamcorper, dui et placerat feugiat, eros pede varius nisi, condimentum viverra felis nunc et lorem. Nam at tortor in tellus interdum sagittis. Morbi ac felis. Etiam ultricies nisi vel augue. Praesent venenatis metus at tortor pulvinar varius. Sed cursus turpis vitae tortor. Donec elit libero, sodales nec, volutpat a, suscipit non, turpis. Fusce vulputate eleifend sapien.</p>
