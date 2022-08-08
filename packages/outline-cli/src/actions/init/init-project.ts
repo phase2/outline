6b95c89981298997a69946ad69f52fd6b835f619
Peter Schuelke <pschuelke@phase2technology.com>
2022-08-08 12:58:56 -0700

    feat: Update outline cli.

--- packages/outline-cli/src/actions/init/init-project.ts
+++ packages/outline-cli/src/actions/init/init-project.ts
@@ -1,33 +1,131 @@
-import { mkdirsSync, copySync } from 'fs-extra';
+/* eslint-disable complexity */
+import {mkdirsSync, copySync} from 'fs-extra'
+import {renameSync, readFileSync, writeFileSync} from 'node:fs'
 import path = require('path');
-import { execSync } from 'node:child_process';
+import {execSync} from 'node:child_process'
+import chalk = require('chalk');
 
+type Prompts = {
+  name: string,
+  starterTemplate: string,
+  gitOrigin: string|null,
+  gitName: string|null,
+  gitDescription: string|null,
+  octane: boolean
+}
 /**
  * Creates story from custom element json file
- * @param {string} args - Args: name of project
- * @param {string} flags - flags: name of the starter package. Defaults to 'default';
+ * @param {string} prompts - prompts: Object of the values from the prompt questions;
  * @returns Nothing, fixing lint warning
  */
-export const initProject = (args: any, flags: any): void => {
-  const projectName = flags.test ? `${args.name}-test` : args.name;
-  const starterTemplate = flags.template || 'default';
-  const nameSpace = `${flags.nameSpace || projectName}`.replace(
+export const initProject = (prompts: Prompts): void => {
+  const nameSpace = prompts.name.replace(
     /[^\dA-Za-z]/g,
-    '-'
-  );
-  const currDir = process.cwd();
-  const resolvedPath = path.resolve(currDir, nameSpace);
+    '-',
+  ).toLowerCase()
+  const currDir = process.cwd()
+  const resolvedPath = path.resolve(currDir, nameSpace)
   const starterPath = path.resolve(
     `${resolvedPath + '/node_modules/@phase2/outline-templates/'}`,
-    starterTemplate
-  );
+    prompts.starterTemplate,
+  )
 
-  mkdirsSync(resolvedPath);
-  process.chdir(resolvedPath);
+  mkdirsSync(resolvedPath)
+  process.chdir(resolvedPath)
+  console.log(`${chalk.blue('info')}: Dowloading Outline ${prompts.starterTemplate} starter template`)
+  execSync('yarn add @phase2/outline-templates', {stdio: [0, 1, 2]})
 
-  execSync('yarn add @phase2/outline-templates', { stdio: [0, 1, 2] });
+  try {
+    copySync(starterPath, resolvedPath)
+  } catch (error) {
+    throw console.error(`${chalk.red('error')}: ${error}`)
+  }
 
-  copySync(starterPath, resolvedPath);
+  console.log(`${chalk.blue('info')}: Installing Outline dependencies`)
+  execSync('yarnpkg', {stdio: [0, 1, 2]})
 
-  execSync('yarnpkg', { stdio: [0, 1, 2] });
-};
+  // NPM does not package .gitignore files. To include it we renamed it. Now rename it correctly.
+  try {
+    renameSync('gitignore', '.gitignore')
+    console.log(`${chalk.green('success')}: Updated gitignore`)
+  } catch (error) {
+    throw console.error(`${chalk.red('error')}: ${error}`)
+  }
+
+  // Set Storybook name
+  // Check for default or other starters that have storybook
+  if (prompts.starterTemplate === 'default') {
+    const themeFile = 'src/.storybook/CustomTheme.js'
+    console.log(`${chalk.blue('info')}: Updating CustomTheme.js`)
+    try {
+      const themeData = readFileSync(themeFile, {encoding: 'utf8', flag: 'r'})
+      const replacedData = themeData.replace(/brandTitle: '.*',/g, `brandTitle: '${prompts.name}',`)
+      writeFileSync(themeFile, replacedData, {encoding: 'utf8'})
+      console.log(`${chalk.green('success')}: CustomTheme Renamed`)
+    } catch (error) {
+      throw console.error(`${chalk.red('error')}: ${error}`)
+    }
+  }
+
+  // set up git
+  if (prompts.gitOrigin) {
+    // Update package.json to use project names.
+    const jsonFile = './package.json'
+    console.log(`${chalk.blue('info')}: Updating package.json`)
+    try {
+      let packageData = readFileSync(jsonFile, {encoding: 'utf8', flag: 'r'}) || null
+
+      if (packageData) {
+        packageData = packageData.replace(/"name": ".*",/, `"name": "${prompts.gitName}",`)
+        packageData = packageData.replace(/"description": ".*",/, `"description": "${prompts.gitDescription}",`)
+
+        writeFileSync(jsonFile, packageData, {encoding: 'utf8'})
+        console.log(`${chalk.green('success')}: package.json updated`)
+      }
+    } catch (error) {
+      throw console.error(`${chalk.red('error')}: ${error}`)
+    }
+
+    // Run git commands for git setup
+    const gitCmd1 = [
+      'git init -b main',
+      `git remote add origin ${prompts.gitOrigin}`,
+      'git add .gitignore',
+      'git commit -m "feat(init): main branch for semantic-release"',
+      'git push origin main',
+      'git checkout -b next',
+      'git push origin next',
+    ]
+
+    console.log(`${chalk.blue('info')}: Initializing git`)
+
+    for (const cmd of gitCmd1) {
+      execSync(cmd, {stdio: [0, 1, 2]})
+      console.log(`${chalk.magenta(cmd)}`)
+    }
+  }
+
+  // Execute octane cli if selected
+  if (prompts.octane) {
+    console.log(`${chalk.blue('info')}: Initializing Octane`)
+    execSync('npx github:phase2/octane-update.git', {stdio: [0, 1, 2]})
+  }
+
+  // Commit all files in project and push to origin
+  if (prompts.gitOrigin) {
+    const gitCmd1 = [
+      'git add .',
+      'git commit -m "feat(init): Outline installation."',
+      'git push origin next',
+    ]
+
+    console.log(`${chalk.blue('info')}: Commiting all files ot Origin`)
+
+    for (const cmd of gitCmd1) {
+      execSync(cmd, {stdio: [0, 1, 2]})
+      console.log(`${chalk.magenta(cmd)}`)
+    }
+  }
+
+  console.log(`${chalk.green('success')}: Outline Installed and Initialized! Happy Coding!`)
+}
--- packages/outline-cli/src/commands/init.ts
+++ packages/outline-cli/src/commands/init.ts
@@ -1,31 +1,103 @@
-import { Command, Flags } from '@oclif/core';
-import { initProject } from '../actions/init/init-project';
+import {Command} from '@oclif/core'
+import * as inquirer from 'inquirer'
+import {execSync} from 'node:child_process'
+
+import {initProject} from '../actions/init/init-project'
 export default class Init extends Command {
   static description = 'Scaffolds a new Outline Project';
 
   static examples = ['<%= config.bin %> <%= command.id %>'];
 
-  static flags = {
-    // flag with a value (-n, --name=VALUE)
-    nameSpace: Flags.string({
-      char: 'n',
-      description:
-        'name space for the project. Defaults to the base name used for the component',
-    }),
-    // flag with no value (-f, --force)
-    force: Flags.boolean({ char: 'f' }),
-    template: Flags.boolean({
-      char: 't',
-      description: 'name of the starter template. Defaults to "default"',
-    }),
-  };
-
-  static args = [
-    { name: 'name', required: true, description: 'name of project' },
-  ];
-
   public async run(): Promise<void> {
-    const { args, flags } = await this.parse(Init);
-    initProject(args, flags);
+    let gitOrigin = null
+    let gitName = null
+    let gitDescription = null
+    let gitSetup = null
+    // const name = await CliUx.ux.prompt('What is your project name? Ex:"Phase2 Outline"')
+    const name = await inquirer.prompt([{
+      name: 'name',
+      message: 'What is your project name? Ex:"Phase2 Outline"',
+      type: 'input',
+      validate: function (input) {
+        if (input) {
+          return true
+        }
+
+        return false
+      },
+    }])
+    const starterTemplate: any = await inquirer.prompt([{
+      name: 'template',
+      message: 'What is your project starter template?',
+      type: 'list',
+      choices: [{name: 'default'}],
+    }])
+
+    gitSetup = await inquirer.prompt([{
+      name: 'gitSetup',
+      message: 'Do you wish to setup git for this project?',
+      type: 'confirm',
+      default: true,
+    }])
+
+    if (gitSetup.gitSetup) {
+      gitName = await inquirer.prompt([{
+        name: 'gitName',
+        message: 'What is your git project name? Ex:"@phase2/outline"',
+        type: 'input',
+        validate: function (input) {
+          if (input) {
+            return true
+          }
+
+          return false
+        },
+      }])
+      gitDescription = await inquirer.prompt([{
+        name: 'gitDescription',
+        message: 'What is your git project description? Ex:"Outline Component Design"',
+        type: 'input',
+        validate: function (input) {
+          if (input) {
+            return true
+          }
+
+          return false
+        },
+      }])
+
+      gitOrigin = await inquirer.prompt([{
+        name: 'gitOrigin',
+        message: 'What is your project git origin? Ex:"git@github.com:phase2/PROJECT-design.git"',
+        type: 'input',
+        validate: function (input) {
+          try {
+            execSync(`git ls-remote ${input} HEAD`)
+            return true
+          } catch (error) {
+            console.log(error)
+            return false
+          }
+        },
+      }])
+    }
+
+    const octane = await inquirer.prompt([{
+      name: 'octane',
+      message: 'Do you want to include Devcloud tooling via Octane?',
+      type: 'confirm',
+      default: true,
+    }])
+
+    // const {args, flags} = await this.parse(Init)
+    const prompts = {
+      name: name.name,
+      starterTemplate: starterTemplate.template,
+      gitOrigin: gitOrigin?.gitOrigin || null,
+      gitName: gitName?.gitName || null,
+      gitDescription: gitDescription?.gitDescription || null,
+      octane: octane.octane,
+    }
+    initProject(prompts)
   }
 }
