3da0af50927fabcc775b26f13cf4d2cf1a8fdc69
Peter Schuelke <pschuelke@phase2technology.com>
2022-08-19 07:27:48 -0700

    feat: expand the url search param functionality.

--- packages/outline-core/src/internal/url.ts
+++ packages/outline-core/src/internal/url.ts
@@ -7,3 +7,22 @@ export function updateUrlSearchParam(param: string, value: string | null) {
   }
   window.history.pushState({}, '', url);
 }
+
+export function getUrlSearchParam(params: Array<string> | []) {
+  const urlParams = new URLSearchParams(document.location.search);
+  const filteredParams: {[key: string]: string | null} = {};
+
+  if(params.length) {
+    // Returns only the requested parameters
+    params.forEach((value: string) => {
+      filteredParams[value] = urlParams.get(value);
+    });
+  } else {
+    // Returns all parameters
+    urlParams.forEach((value, key) => {
+      filteredParams[key] = value;
+    });
+    return urlParams;
+  }
+  return filteredParams;
+}
