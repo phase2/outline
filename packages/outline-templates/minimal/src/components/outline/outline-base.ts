2008af21b4a412dd3669c0feb5a17ae2c4e0cec4
Peter Schuelke <pschuelke@phase2technology.com>
2022-08-19 07:24:19 -0700

    feat: rename and add new build packages.

---
+++ packages/outline-templates/minimal/src/components/outline/outline-base.ts
@@ -0,0 +1,10 @@
+import { OutlineContainer } from '@phase2/outline-container';
+import { OutlineGrid, OutlineColumn } from '@phase2/outline-grid';
+import { OutlineLink } from '@phase2/outline-link';
+import { OutlineCodeBlock } from '@phase2/outline-code-block';
+
+new OutlineContainer();
+new OutlineGrid();
+new OutlineColumn();
+new OutlineLink();
+new OutlineCodeBlock();
