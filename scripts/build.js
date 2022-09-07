6e720ba1fc1a1a29c9e72e6c3075f68bf0b3d289
Jake Strawn <jake.strawn@gmail.com>
2022-09-07 17:35:46 -0400

    fix: Updating some packages for local builds/consumption

--- scripts/build.js
+++ scripts/build.js
@@ -2,11 +2,13 @@ const { series } = require('async');
 const { exec } = require('child_process');
 
 series([
+  // Remove any prior build assets.
   callback => exec('rm -rf ./dist', callback),
+  // Build the client-side assets.
   callback => exec('tsc -p tsconfig.build.json', callback),
-  callback =>
-    exec(
-      "rsync -avm --include '*/' --include '*.css' --exclude '*' . dist",
-      callback
-    ),
+  // callback =>
+  //   exec(
+  //     "rsync -avm --remove-source-files --include '*.js' --include '*.css' --exclude '*' . dist",
+  //     callback
+  //   ),
 ]);
