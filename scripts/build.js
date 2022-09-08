c1ab3a263169e0980e443c257f7ccdcfbee9bfb0
Jake Strawn <jake.strawn@gmail.com>
2022-09-08 12:20:47 -0400

    fix: Updating build procedures.

--- scripts/build.js
+++ scripts/build.js
@@ -5,10 +5,15 @@ series([
   // Remove any prior build assets.
   callback => exec('rm -rf ./dist', callback),
   // Build the client-side assets.
+  callback =>
+    exec(
+      'esbuild index.ts --bundle --outdir=dist --tsconfig=tsconfig.build.json',
+      callback
+    ),
   callback => exec('tsc -p tsconfig.build.json', callback),
-  // callback =>
-  //   exec(
-  //     "rsync -avm --remove-source-files --include '*.js' --include '*.css' --exclude '*' . dist",
-  //     callback
-  //   ),
+  callback =>
+    exec(
+      "rsync -avm --remove-source-files --include '*.js' --include '*.css' --exclude '*' . dist",
+      callback
+    ),
 ]);
