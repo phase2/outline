1d19389018e277181f567c86ce646737b3f756e3
Jake Strawn <jake.strawn@gmail.com>
2023-02-27 13:53:06 -0500

    fix(docs): Updating default documentation.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,20 @@
 module.exports = {
-  stories: ['../packages/outline-storybook/stories/**/*.stories.@(js|ts|mdx)'],
+  stories: [
+    // Intentionally ordering welcome page first.
+    '../packages/outline-docs/src/guides/welcome.mdx',
+    // Component development guides.
+    '../packages/outline-docs/src/guides/development/component-development/**/*.mdx',
+    // Component usage guides.
+    '../packages/outline-docs/src/guides/consumers/**/*.mdx',
+    // QA/UAT usage guides.
+    '../packages/outline-docs/src/guides/qa-uat/**/*.mdx',
+    // Tooling usage guides.
+    '../packages/outline-docs/src/guides/tooling/**/*.mdx',
+    // Documentation stories.
+    '../packages/outline-docs/src/guides/**/*.stories.@(js|ts|mdx)',
+    // Component stories.
+    '../packages/outline-storybook/stories/**/*.stories.@(js|ts|mdx)',
+  ],
   typescript: {
     check: false,
     checkOptions: {},
@@ -21,6 +36,6 @@ module.exports = {
   },
   docs: {
     autodocs: true,
-    defaultName: 'Full Documentation', // set to change the name of generated docs entries
+    defaultName: 'Documentation', // set to change the name of generated docs entries
   },
 };
