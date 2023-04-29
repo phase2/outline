20a44131076052dd72059287c5c0331939481b87
Chris Johnson <cjohnson@phase2technology.com>
2023-04-29 19:36:25 -0400

    feat: Adjusting outline-docs package so projects can use them
    Also changes the template for spinning up new projects

--- packages/outline-docs/index.ts
+++ packages/outline-docs/index.ts
@@ -1,1 +1,12 @@
-export default {};
+export const outlineStories = [
+  // Intentionally ordering welcome page first.
+  `${__dirname}/src/guides/welcome.mdx`,
+  // Component development guides.
+  `${__dirname}/src/guides/development/component-development/**/*.mdx`,
+  // Component usage guides.
+  `${__dirname}/src/guides/consumers/**/*.mdx`,
+  // QA/UAT usage guides.
+  `${__dirname}/src/guides/qa-uat/**/*.mdx`,
+  // Tooling usage guides.
+  `${__dirname}/src/guides/tooling/**/*.mdx`,
+];
