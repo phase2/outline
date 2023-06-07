a98587c944b8290d4ed42bb6c310121110b656e4
Jake Strawn <jake.strawn@gmail.com>
2023-06-07 10:13:57 -0400

    fix(outline-cli): Fix for eslint tests.

--- packages/tools/outline-cli/src/actions/init/init-project.ts
+++ packages/tools/outline-cli/src/actions/init/init-project.ts
@@ -1,7 +1,8 @@
-import {mkdirsSync, copySync} from 'fs-extra'
-import {renameSync, readFileSync, writeFileSync} from 'node:fs'
+/* eslint-disable unicorn/prefer-module */
+import { mkdirsSync, copySync } from 'fs-extra';
+import { renameSync, readFileSync, writeFileSync } from 'node:fs';
 import path = require('path');
-import {execSync} from 'node:child_process'
+import { execSync } from 'node:child_process';
 import chalk = require('chalk');
 
 export type Prompts = {
@@ -17,101 +18,101 @@ export type Prompts = {
  * @returns {void}
  */
 export const initProject = (prompts: Prompts, local = false): void => {
-  const currDir = process.cwd()
-  const resolvedPath = path.resolve(currDir, prompts.slug)
-  const storybookSource = local ?
-    `${__dirname}/../../../../outline-storybook` :
-    path.resolve(resolvedPath, './node_modules/@phase2/outline-storybook')
-  const starterPath = local ?
-    `${__dirname}/../../../../outline-templates/${prompts.template}` :
-    path.resolve(
-      `${resolvedPath + '/node_modules/@phase2/outline-templates/'}`,
-      prompts.template,
-    )
+  const currDir = process.cwd();
+  const resolvedPath = path.resolve(currDir, prompts.slug);
+  const storybookSource = local
+    ? `${__dirname}/../../../../outline-storybook`
+    : path.resolve(resolvedPath, './node_modules/@phase2/outline-storybook');
+  const starterPath = local
+    ? `${__dirname}/../../../../outline-templates/${prompts.template}`
+    : path.resolve(
+        `${resolvedPath + '/node_modules/@phase2/outline-templates/'}`,
+        prompts.template
+      );
 
-  mkdirsSync(resolvedPath)
-  process.chdir(resolvedPath)
-  console.log(`${chalk.blue('info')}: Moved to directory: ${resolvedPath}`)
+  mkdirsSync(resolvedPath);
+  process.chdir(resolvedPath);
+  console.log(`${chalk.blue('info')}: Moved to directory: ${resolvedPath}`);
 
   if (!local) {
     console.log(
       `${chalk.bold.blue('info')}: Downloading Outline ${
         prompts.template
-      } starter template`,
-    )
+      } starter template`
+    );
     execSync(
-      'yarn add @phase2/outline-templates @phase2/outline-storybook --cwd=\'./\'',
+      "yarn add @phase2/outline-templates @phase2/outline-storybook --cwd='./'",
       {
         stdio: [0, 1, 2],
-      },
-    )
+      }
+    );
   }
 
   // Move the default files to the root directory.
   try {
-    copySync(starterPath, resolvedPath)
+    copySync(starterPath, resolvedPath);
   } catch (error) {
-    throw console.error(`${chalk.red('error')}: ${error}`)
+    throw console.error(`${chalk.red('error')}: ${error}`);
   }
 
   try {
-    mkdirsSync(`${resolvedPath}/src/.storybook`)
-    mkdirsSync(`${resolvedPath}/src/.storybook/stories`)
-    copySync(`${storybookSource}/config`, './src/.storybook')
-    copySync(`${storybookSource}/stories`, './src/.storybook/stories')
+    mkdirsSync(`${resolvedPath}/src/.storybook`);
+    mkdirsSync(`${resolvedPath}/src/.storybook/stories`);
+    copySync(`${storybookSource}/config`, './src/.storybook');
+    copySync(`${storybookSource}/stories`, './src/.storybook/stories');
   } catch (error) {
-    throw console.error(`${chalk.red('error')}: ${error}`)
+    throw console.error(`${chalk.red('error')}: ${error}`);
   }
 
   if (!local) {
-    console.log(`${chalk.blue('info')}: Installing Outline dependencies`)
-    execSync('yarnpkg', {stdio: [0, 1, 2]})
+    console.log(`${chalk.blue('info')}: Installing Outline dependencies`);
+    execSync('yarnpkg', { stdio: [0, 1, 2] });
   }
 
   // NPM does not package .gitignore files. To include it we renamed it. Now rename it correctly.
   try {
-    renameSync('gitignore', '.gitignore')
-    console.log(`${chalk.green('success')}: Updated gitignore`)
+    renameSync('gitignore', '.gitignore');
+    console.log(`${chalk.green('success')}: Updated gitignore`);
   } catch (error) {
-    throw console.error(`${chalk.red('error')}: ${error}`)
+    throw console.error(`${chalk.red('error')}: ${error}`);
   }
 
   // Update package.json to use project names.
-  const jsonFile = './package.json'
-  console.log(`${chalk.blue('info')}: Updating package.json`)
+  const jsonFile = './package.json';
+  console.log(`${chalk.blue('info')}: Updating package.json`);
   try {
     let packageData =
-      readFileSync(jsonFile, {encoding: 'utf8', flag: 'r'}) || null
+      readFileSync(jsonFile, { encoding: 'utf8', flag: 'r' }) || null;
 
     if (packageData) {
       packageData = packageData.replace(
         /"name": ".*",/,
-        `"name": "${prompts.packageName}",`,
-      )
+        `"name": "${prompts.packageName}",`
+      );
       packageData = packageData.replace(
         /"description": ".*",/,
-        `"description": "${prompts.description}",`,
-      )
+        `"description": "${prompts.description}",`
+      );
 
-      writeFileSync(jsonFile, packageData, {encoding: 'utf8'})
-      console.log(`${chalk.green('success')}: package.json updated`)
+      writeFileSync(jsonFile, packageData, { encoding: 'utf8' });
+      console.log(`${chalk.green('success')}: package.json updated`);
     }
   } catch (error) {
-    throw console.error(`${chalk.red('error')}: ${error}`)
+    throw console.error(`${chalk.red('error')}: ${error}`);
   }
 
   if (local) {
     console.log(
-      `${chalk.blue('info')}: Linking outline packages for local development`,
-    )
+      `${chalk.blue('info')}: Linking outline packages for local development`
+    );
     execSync(`node scripts/link-for-local-dev.js ${__dirname}/../../../..`, {
       stdio: [0, 1, 2],
-    })
+    });
   }
 
   console.log(
     `${chalk.green(
-      'success',
-    )}: Outline installed and initialized! Happy Coding!`,
-  )
-}
+      'success'
+    )}: Outline installed and initialized! Happy Coding!`
+  );
+};
