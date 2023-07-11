1cb23dc500b4ae5835b52ad9b8282c9a7da36d99
Jake Strawn <jake.strawn@gmail.com>
2023-07-11 10:18:44 -0400

    fix(scripts): Fix for CSS generation in styles.mjs.

--- scripts/styles.mjs
+++ scripts/styles.mjs
@@ -82,24 +82,6 @@ const createComponentLiterals = (result, path) => {
     `
 import { css } from 'lit';
 export default css\`
-/* Apply standardized box sizing to the component. */
-:host {
-  box-sizing: border-box;
-}
-:host *,
-:host *::before,
-:host *::after {
-  box-sizing: inherit;
-}
-/* Apply proper CSS for accessibly hiding elements to each component. */
-.visually-hidden {
-  position: absolute !important;
-  overflow: hidden;
-  clip: rect(1px, 1px, 1px, 1px);
-  width: 1px;
-  height: 1px;
-  word-wrap: normal;
-}
 /* Apply component specific CSS */
 ${result.css}\`;`,
     () => true
