7e1737fc0ac67b5ba78efbb5e0fa6c7a3d76671a
bardleb <bjones@phase2technology.com>
2023-10-03 07:42:41 -0400

    fix: core button changes

---
+++ packages/components/outline-core-button/index.ts
@@ -0,0 +1,2 @@
+export { OutlineCoreButton } from './src/outline-core-button';
+export type { ButtonVariant, ButtonSize } from './src/outline-core-button';
---
+++ packages/components/outline-extended-button/outline-extended-button.ts
@@ -0,0 +1,16 @@
+import { OutlineCoreButton } from "@phase2/outline-core-button";
+import { customElement } from 'lit/decorators.js';
+import "@phase2/outline-core-button";
+// Import @phase2 version breaking Storybook
+// import { buttonVariantsTypes } from "@phase2/outline-core-button/src/outline-core-button";
+import { buttonVariantsTypes } from "../outline-core-button/src/outline-core-button";
+
+buttonVariantsTypes.push('shiny');
+
+// Rewriting does not work
+// const buttonVariantsTypes = ['shiny', 'dull', 'matte'];
+
+@customElement('outline-extended-button')
+export class OutlineExtendedButton extends OutlineCoreButton {
+    
+}
