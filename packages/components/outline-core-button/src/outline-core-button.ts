e6a12adc9cfef8612c09ee272aa9375019589ecb
Jake Strawn <jake.strawn@gmail.com>
2023-12-05 15:40:07 -0500

    feat(outline-core-button): Tweaks for release prep.

--- packages/components/outline-core-button/src/outline-core-button.ts
+++ packages/components/outline-core-button/src/outline-core-button.ts
@@ -2,35 +2,37 @@ import { CSSResultGroup, html, TemplateResult } from 'lit';
 import { customElement, property, state } from 'lit/decorators.js';
 import { OutlineElement } from '@phase2/outline-core';
 import { AdoptedStyleSheets } from '@phase2/outline-adopted-stylesheets-controller';
-import { linkVars } from '@phase2/outline-link';
 import componentStyles from './outline-core-button.css.lit';
 import globalStyles from './outline-core-button.lightdom.css.lit';
 
 export const buttonVariantsTypes = ['primary', 'secondary', 'tertiary'];
 export const buttonSizeTypes = ['small', 'medium', 'large'];
 
-// export type ButtonVariant = keyof typeof buttonVariantsTypes;
 export type ButtonVariant = (typeof buttonVariantsTypes)[number];
 export type ButtonSize = keyof typeof buttonSizeTypes;
 
 /**
  * The Outline Core Button component
  * @element outline-core-button
- * @since 1.0.0
+ * @since 0.0.1
+ *
+ * @prop {string} buttonUrl - The url to use for a link. This will render an anchor element.
+ * @prop {string} buttonTarget - The target to use for a link, used in conjunction with the url attribute.
+ * @prop {string} buttonLabel - ARIA label attribute to pass down to the resulting button or a element.
+ * @prop {string} buttonVariant - The button variant. One of: link, primary, secondary, tertiary.
+ * @prop {string} buttonSize - The button size. One of: small, medium, large.
+ * @prop {boolean} isDisabled - Whether the button is disabled.
  * @attr {string} button-url - The url to use for a link. This will render an anchor element.
  * @attr {string} button-target - The target to use for a link, used in conjunction with the url attribute.
  * @attr {string} button-label -ARIA label attribute to pass down to the resulting button or a element.
  * @attr {string} button-variant - The button variant. One of: link, primary, secondary, tertiary.
  * @attr {string} button-size - The button size. One of: small, medium, large.
  * @attr {boolean} is-disabled - Whether the button is disabled.
- * @slot - default slot, used for button text.
- * @slot left - slot used for left side icon.
- * @slot right - slot used for right side icon.
- * @todo Convert styles to utilize CSS Variables.
+ * @slot - default slot, used for button text and any related visual content (icon).
  */
 @customElement('outline-core-button')
 export class OutlineCoreButton extends OutlineElement {
-  static styles: CSSResultGroup = [linkVars, componentStyles];
+  static styles: CSSResultGroup = [componentStyles];
   AdoptedStyleSheets: AdoptedStyleSheets;
 
   @state() buttonsVariantList = buttonVariantsTypes;
@@ -87,7 +89,7 @@ export class OutlineCoreButton extends OutlineElement {
       if (buttonSize && !Object.values(buttonSizeTypes).includes(buttonSize)) {
         buttonSize = buttonSizeTypes[0];
       }
-      return buttonSize;
+      return buttonSize as String;
     },
   })
   buttonSize: ButtonSize;
@@ -134,7 +136,6 @@ export class OutlineCoreButton extends OutlineElement {
   }
 
   updated() {
-    // TODO: Debug slot controller application.
     // checks the is-disabled prop and manages aria-disabled attributes on the <outline-core-button> element itself.
     if (this.hasAttribute('is-disabled')) {
       this.setAttribute('aria-disabled', 'true');
